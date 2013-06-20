df1c04c8d3ead153d87fa1223ae87a0544031fa3
Nicholas Long <nicholas.long@nrel.gov>
2013-06-19 22:45:53 -0600

    initial commit with aws plugin

---
+++ chef/roles/openstudio-server.rb
@@ -0,0 +1,59 @@
+name "openstudio-server"
+description "Install and configure OpenStudio for use with Ruby on Rails"
+
+run_list([
+             # Default iptables setup on all servers.
+             "recipe[apt]",
+             "recipe[build-essential]",
+             "recipe[ruby_build]",
+             "recipe[rbenv::system]",
+             "recipe[rbenv::vagrant]",
+             "recipe[openstudio]",
+             "recipe[energyplus]",
+             "recipe[R]",
+             "recipe[mongodb::server]",
+         ])
+
+
+default_attributes(
+    'rbenv' => {
+        'rubies' => [ '1.9.3-p392'],
+        #'global' => '1.9.3-p392',
+        'gems' => {
+            '1.8.7-p371' => [
+                { 'name' => 'bundler'},
+                { 'name' => 'rake'},
+                { 'name' => 'rubygems-bundler'}
+            ],
+            '1.9.3-p392' => [
+                { 'name' => 'bundler'},
+                { 'name' => 'rake'},
+                { 'name' => 'rails'},
+                { 'name' => 'rubygems-bundler'}
+            ]
+        }
+    },
+
+    :openstudio => {
+        :version => "0.10.5.11322",
+        :checksum => "9180659c77a7fc710cb9826d40ae67c65db0d26bb4bce1a93b64d7e63f4a1f2c"
+    },
+    :energyplus => {
+        :version => "7.2.0.006",
+        :checksum => "c1ec1499f964bad8638d3c732c9bd10793dd4052a188cd06bb49288d3d962e09"
+    },
+    'R' => {
+        'source_url' => "http://cran.r-project.org/src/contrib",
+        'packages' => [
+            {
+                'name' => 'lhs',
+                'version' => '0.10' },
+            {
+                'name' => 'Rserve',
+                'version' => '0.6-8.1' },
+            {
+                'name' => 'triangle',
+                'version' => '0.8' }
+        ]
+    }
+)
