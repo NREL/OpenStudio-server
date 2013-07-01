ab4984edc6f4d13b4f25f0b41a04ff2cadf5dc15
Nicholas Long <nicholas.long@nrel.gov>
2013-07-01 07:44:58 -0600

    initial rails project

---
+++ openstudio-server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenstudioServer::Application.config.secret_token = '92148dffd90186a4d05788e0ee1c9203b69e3b9314785431a8959ef5a6aad72f62328a9af5afb3d187f35e62a7e648e52ad97999e644e57b171d48b69e7e5e3a'
