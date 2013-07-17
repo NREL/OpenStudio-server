f3e53dabb2e3709bf5dc0567dac90d7f26b64330
Nicholas Long <nicholas.long@nrel.gov>
2013-07-17 13:29:26 -0600

    adding Rserve gem and example method to call it

--- openstudio-server/app/models/analysis.rb
+++ openstudio-server/app/models/analysis.rb
@@ -17,6 +17,28 @@ class Analysis
 
   def start_r_and_run_sample()
 
+    # determine which problem to run
+
+    # add into delayed job
+    require 'rserve/simpler'
+
+    #create an instance for R
+    @r = Rserve::Simpler.new
+
+    @r.command() do
+      %Q{
+        f <- function(x){
+          x1<-x[1]
+          x2<-x[2]
+          z <- (x1-1)*(x1-1) + (x2-1)*(x2-1)
+          as.numeric(z)}
+
+          b<-optim(c(1.5,4),f,method='L-BFGS-B',lower=c(-5,-5), upper=c(5,5))
+        }
+    end
+
+    @r.converse('b')
+
 
   end
 
---
+++ openstudio-server/spec/models/analysis_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Analysis do
+  before :each do
+    #@project = FactoryGirl.create :project
+    @analysis = Analysis.find_by(name: "PAT Example")
+  end
+
+  it "should have an analysis to run" do
+    Analysis.all.size.should eq(1), 'does not have an analysis'
+  end
+
+  it "should return a value from R" do
+    result = @analysis.start_r_and_run_sample()
+
+    puts result.inspect
+
+  end
+
+  after :each do
+  end
+end
