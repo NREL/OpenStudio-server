fa8a57ba3c166e14f4e40e1798bfae7b6f1b18f6
Nicholas Long <nicholas.long@nrel.gov>
2013-07-22 22:49:28 -0600

    breaking up roles and adding passenger and other cookbooks

---
+++ vagrant/chef/roles/web_base.rb
@@ -0,0 +1,25 @@
+name "buildingagent_web_base"
+description "A base role for buildingagent web servers."
+
+run_list([
+  "role[base]",
+  "role[ruby]",
+
+  "recipe[apache2]",
+  "recipe[apache2::mod_rewrite]",
+  "recipe[apache2::mod_ssl]",
+  "recipe[apache2::iptables]",
+  "role[passenger_apache]",
+
+  #"recipe[buildingagent::web]",
+
+  "recipe[deploy_permissions]",
+  "recipe[deploy_permissions::apache]",
+  "recipe[deploy_permissions::whenever]",
+])
+
+override_attributes({
+  :apache => {
+    :listen_ports => ["80", "443"],
+  },
+})
