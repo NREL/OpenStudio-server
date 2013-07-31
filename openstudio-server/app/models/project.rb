e89fcc300f85c62737485913651487b50f424507
Nicholas Long <nicholas.long@nrel.gov>
2013-07-31 11:09:07 -0600

    forcing uuid to exist and saving analysis

--- openstudio-server/app/models/project.rb
+++ openstudio-server/app/models/project.rb
@@ -12,7 +12,9 @@ class Project
 
   def create_single_analysis(analysis_name, problem_name)
     analysis = self.analyses.find_or_create_by(name: analysis_name)
+    analysis.uuid = UUID.new.generate()  #needs a unique id
     puts analysis.inspect
+    analysis.save!
     problem = analysis.problems.find_or_create_by(name: problem_name)
   end
 
