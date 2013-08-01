bd3d8b1d9cd681fca9654854c13fe07a4b489cfa
Nicholas Long <nicholas.long@nrel.gov>
2013-07-31 21:57:20 -0600

    updating roles pulling out R and bumping versions

--- vagrant/chef/roles/openstudio-server.rb
+++ vagrant/chef/roles/openstudio-server.rb
@@ -6,47 +6,20 @@ run_list([
              "role[base]",
              "role[ruby]",
              "role[web_base]",
+             "role[r-project]",
              "recipe[openstudio]",
              "recipe[energyplus]",
-             "recipe[R]",
              "recipe[mongodb::server]",
          ])
 
 
 default_attributes(
     :openstudio => {
-        :version => "0.10.5.11322",
-        :checksum => "9180659c77a7fc710cb9826d40ae67c65db0d26bb4bce1a93b64d7e63f4a1f2c"
+        :version => "1.0.2.efb742a74c",
+        #:checksum => "9180659c77a7fc710cb9826d40ae67c65db0d26bb4bce1a93b64d7e63f4a1f2c"
     },
     :energyplus => {
-        :version => "7.2.0.006",
-        :checksum => "c1ec1499f964bad8638d3c732c9bd10793dd4052a188cd06bb49288d3d962e09"
-    },
-    'R' => {
-        'apt_distribution' => "precise/",
-        'apt_key' => "E084DAB9",
-        'package_source_url' => "http://cran.r-project.org/src/contrib",
-        'packages' => [
-            {
-                'name' => 'lhs',
-                'version' => '0.10'
-            },
-            {
-                'name' => 'Rserve',
-                'version' => '0.6-8.1'
-            },
-            {
-                'name' => 'triangle',
-                'version' => '0.8'
-            },
-            {
-                'name' => 'snow',
-                'version' => '0.3-12'
-            },
-            {
-                'name' => 'snowfall',
-                'version' => '1.84-4'
-            }
-        ]
+        :version => "800008",
+        #:checksum => "c1ec1499f964bad8638d3c732c9bd10793dd4052a188cd06bb49288d3d962e09"
     }
 )
