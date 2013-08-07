9b42f772ea8419b643d68f48c50d9d3e8a178f14
Nicholas Long <nicholas.long@nrel.gov>
2013-08-07 14:56:37 -0600

    adding java for r dependency

--- vagrant/chef/roles/r-project.rb
+++ vagrant/chef/roles/r-project.rb
@@ -2,6 +2,7 @@ name "r-project"
 description "Installs and configure R"
 
 run_list([
+             "recipe[java]",
              "recipe[R]",
          ])
 
@@ -11,6 +12,8 @@ default_attributes(
         'apt_distribution' => "precise/",
         'apt_key' => "E084DAB9",
         'package_source_url' => "http://cran.r-project.org/src/contrib",
+        'add_ld_path' => true,
+        'java_libjvm' => "/usr/lib/jvm/java-6-openjdk-amd64/jre/lib/amd64/server/libjvm.so",
         'packages' => [
             {
                 'name' => 'lhs',
@@ -18,11 +21,19 @@ default_attributes(
             },
             {
                 'name' => 'Rserve',
-                'version' => '0.6-8.1'
+                'version' => '1.7-1'
             },
             {
                 'name' => 'triangle',
                 'version' => '0.8'
+            },
+            {
+                'name' => 'rJava',
+                'version' => '0.9-4'
+            },
+            {
+                'name' => 'RUnit',
+                'version' => '0.4.26'
             },
             {
                 'name' => 'RMongo',
@@ -35,3 +46,6 @@ default_attributes(
         ]
     }
 )
+
+
+
