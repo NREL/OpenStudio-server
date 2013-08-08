180c6c95d2f149e48e7517f4cdb5bd59a90abe4c
Elaine Hale <elaine.hale@nrel.gov>
2013-08-08 13:14:08 -0600

    pat_to_json.rb uses new SDK JSON serialization functionality to create initial test structure for worker node workflows.
    Removing hand-copied SDK JSON files in favor of serializing the objects in PATTest in a folder structure amenable for testing SimulateDataPoint.rb.
    
    To re-create these files, run pat_to_json.rb while -I pointing to a current build of the OpenStudio develop branch.

--- prototype/pat/pat_to_json.rb
+++ prototype/pat/pat_to_json.rb
@@ -8,20 +8,19 @@ def print_uuid(uuid)
   return uuid.to_s.gsub('}','').gsub('{','')
 end
 
+# Open and retrieve data from example project
 OpenStudio::Application::instance::application
-
 pat_path = OpenStudio::Path.new(File.dirname(__FILE__) + "/PATTest")
-
 project = OpenStudio::AnalysisDriver::SimpleProject::open(pat_path).get
-
 analysis = project.analysis
-
 problem = analysis.problem
-
 variables = problem.variables
-
 seed_data_point = analysis.dataPoints[0]
+datapoints = analysis.dataPoints
 
+# Print server views ###########################################################
+
+# server_problem.json
 variables_array = []
 variables.each_index do |i|
   variable = variables[i]
@@ -33,14 +32,14 @@ variables.each_index do |i|
   variable_hash["name"] = variable.name
   variable_hash["display_name"] = variable.displayName
   
-  discrete_perturbation = variable.to_DiscreteVariable.get
+  measure_group = variable.to_MeasureGroup.get
 
-  # DLM: do we want only selected perturbations?  Would this be better as an enum?
-  perturbations = discrete_perturbation.perturbations(false)
+  # DLM: do we want only selected measures?  Would this be better as an enum?
+  measures = measure_group.measures(false)
   
   variable_hash["type"] = "Integer"
   variable_hash["minimum"] = 0
-  variable_hash["maximum"] = perturbations.size - 1
+  variable_hash["maximum"] = measures.size - 1
   variable_hash["initial_value"] = seed_data_point.variableValues[i].toInt
 
   variables_array << variable_hash
@@ -55,12 +54,9 @@ File.open('server_problem.json', 'w') do |file|
   file.puts result
 end
 
-#########################################################
-
-datapoints = analysis.dataPoints
+# server_datapoints_request.json
 
 datapoints_array = []
-
 datapoints.each do |data_point|
   data_point_hash = OrderedHash.new
   data_point_hash["uuid"] = print_uuid(data_point.uuid)
@@ -93,3 +89,43 @@ File.open('server_datapoints_request.json', 'w') do |file|
   file.puts result
 end
 
+# Create and populate folders for individual DataPoint runs ####################
+
+require 'fileutils'
+
+# Set up project folder and files
+project_dir_name = "analysis_" + print_uuid(analysis.uuid)
+if File.exist?(project_dir_name)
+  FileUtils.rm_rf(project_dir_name)
+end
+FileUtils.mkdir(project_dir_name)
+project_dir_path = OpenStudio::Path.new(project_dir_name)
+#
+# ETH@20130808: The following code should be replaced by some sort of .tar.gz export of SimpleProject
+# that is called and posted to openstudio-server by the RemoteAnalysisDriver. 
+#
+# seed.osm and seed/files/
+FileUtils.cp(project.seed.path.to_s,(project_dir_path / OpenStudio::Path.new("seed.osm")).to_s)
+FileUtils.mkdir((project_dir_path / OpenStudio::Path.new("seed")).to_s)
+FileUtils.cp_r((project.seed.path.parent_path / 
+                OpenStudio::Path.new(project.seed.path.stem) / 
+                OpenStudio::Path.new("files")).to_s,
+               (project_dir_path / OpenStudio::Path.new("seed/files")).to_s)
+# scripts
+FileUtils.cp_r((pat_path / OpenStudio::Path.new("scripts")).to_s,
+               (project_dir_path / OpenStudio::Path.new("scripts")).to_s)
+# problem formulation
+formulation_json_path = project_dir_path / OpenStudio::Path.new("formulation.json")
+analysis.saveJSON(formulation_json_path,"ProblemFormulation".to_AnalysisSerializationScope)
+
+# Set up run folders and files
+datapoints.each do |data_point|
+
+  data_point_dir_name = "data_point_" + print_uuid(data_point.uuid)
+  data_point_dir_path = project_dir_path / OpenStudio::Path.new(data_point_dir_name)
+  FileUtils.mkdir(data_point_dir_path.to_s)
+  
+  data_point_json_path = data_point_dir_path / OpenStudio::Path.new("data_point_in.json")  
+  data_point.saveJSON(data_point_json_path)  
+  
+end
