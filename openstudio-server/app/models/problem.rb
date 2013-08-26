5200f34357fdd7a89268d97caad2bff89aabed2f
Nicholas Long <nicholas.long@nrel.gov>
2013-08-26 01:10:35 -0600

    removign server_veiw element in hash

--- openstudio-server/app/models/problem.rb
+++ openstudio-server/app/models/problem.rb
@@ -12,7 +12,7 @@ class Problem
   has_many :variables
 
   def load_variables_from_pat_json(data)
-    data[:metadata][:server_view][:variables].each do |datum|
+    data[:metadata][:variables].each do |datum|
       puts datum.inspect
       variable = self.variables.find_or_create_by(uuid: datum[:uuid])
 
