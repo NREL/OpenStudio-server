dc00bc48eb9afa03403fb29eb3ace95f6e67920d
Dan Macumber <dan.macumber@gmail.com>
2013-08-26 15:28:33 -0600

    Updating os test scripts

--- prototype/example_scripts/vagrantup.rb
+++ prototype/example_scripts/vagrantup.rb
@@ -8,8 +8,15 @@ workerPath = OpenStudio::Path.new('C:\working\openstudio-server\vagrant\worker')
 workerUrl = OpenStudio::Url.new("http://localhost:8081")
 
 vagrantProvider = OpenStudio::VagrantProvider.new(serverPath, serverUrl, workerPath, workerUrl)
+
+vagrantProvider.signUserAgreement(true)
+
+vagrantProvider.internetAvailable
+
 vagrantProvider.serviceAvailable
+
 vagrantProvider.startServer
+
 #vagrantProvider.startWorkers
 
 puts "starting"
@@ -25,15 +32,20 @@ server = OpenStudio::OSServer.new(vagrantProvider.serverUrl.get)
 
 while (not server.available)
   OpenStudio::System::msleep(3000)
-  puts "waiting for server"
+  puts "waiting for server to start"
 end
 
-puts "available"
+puts "server available"
 
 puts server.projectUUIDs
 
 puts "shutting down"
 
 vagrantProvider.terminate
+
+while (not vagrantProvider.terminateComplete)
+  OpenStudio::System::msleep(3000)
+  puts "waiting to shut down"
+end
 
 puts "goodbye"
