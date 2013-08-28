4b12c4a60d52eeb6f17961a34503aa11de1e6cd7
brian ball <brian.ball@nrel.gov>
2013-08-28 13:01:22 -0600

    remote R works on EC2 as well just run >ruby runClass.rb

--- launch-instance/classAWS.rb
+++ launch-instance/classAWS.rb
@@ -243,6 +243,45 @@ module AwsInterface
       # empty instances
       @instances_slave = Array.new(0)
     end
+    
+#======================= send command ======================#
+    # Send a command through SSH Shell to an instance. 
+    # Need to pass instance object and the command as a string.     
+def shell_command(instance, command)
+  puts "executing shell command #{command}"
+  begin
+  Net::SSH.start(instance.ip_address, "ubuntu",
+                 :key_data => [@key_pair.private_key]) do |ssh|
+    channel = ssh.open_channel do |ch|
+      ch.exec "#{command}" do |ch, success|
+        raise "could not execute #{command}" unless success
+
+        # "on_data" is called when the process writes something to stdout
+        ch.on_data do |c, data|
+          $stdout.print data
+        end
+
+        # "on_extended_data" is called when the process writes something to stderr
+        ch.on_extended_data do |c, type, data|
+          $stderr.print data
+        end
+
+        ch.on_close { puts "done!" }
+      end
+    end
+  end
+  rescue Net::SSH::HostKeyMismatch => e
+     e.remember_host!
+     puts "key mismatch, retry"
+     sleep 1
+     retry
+  rescue SystemCallError, Timeout::Error => e
+     # port 22 might not be available immediately after the instance finishes launching
+     sleep 1
+     # puts "Not Yet"
+     retry
+  end
+end      
 
 #======================= send command ======================#
     # Send a command through SSH to an instance. 
