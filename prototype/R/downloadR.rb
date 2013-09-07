f318a6f24e80a1a34c41448fed9b2e0f16c8a978
brian ball <brian.ball@nrel.gov>
2013-09-07 17:39:24 -0600

    removing hard coded ips

--- prototype/R/downloadR.rb
+++ prototype/R/downloadR.rb
@@ -30,23 +30,30 @@ lines.each do |line|
   puts dp.ip_address
   puts dp.uuid
   puts dp.values
+  adir = dp.analysis_id
+  analysis_dir = "/home/vagrant/analysis_" << adir[1...-1]
+  if Dir.exists?(analysis_dir) == false
+    Dir.mkdir(analysis_dir)
+  end  
   
   # zip datapoint File
   datapoint_path = "data_point_" << line[0...-1]
   datapoint_path_zip = "data_point_" << line[0...-1] << ".zip"
   command = "cd /home/vagrant/analysis ; zip -r " << datapoint_path_zip << " " << datapoint_path
-  a.shell_command("192.168.33.11",command)
+  a.shell_command(dp.ip_address,command)
   
   # download datapoint
   local_path = "/home/vagrant/analysis/" << datapoint_path_zip 
   remote_path = "/home/vagrant/analysis/" << datapoint_path_zip
   # download File to slave Instance
-  a.download_file("192.168.33.11", remote_path, local_path)
+  a.download_file(dp.ip_address, remote_path, local_path)
   command = "chmod 774 " << local_path
-  a.send_command("192.168.33.10",command)
+  `#{command}`
+  #a.send_command("192.168.33.10",command)
   
   # Unzip Analysis Zip File
   command = "unzip " << "/home/vagrant/analysis/" << datapoint_path_zip << " -d " << "/home/vagrant/analysis/"
-  a.send_command("192.168.33.10",command)
+  `#{command}`
+  #a.send_command("192.168.33.10",command)
   
 end
