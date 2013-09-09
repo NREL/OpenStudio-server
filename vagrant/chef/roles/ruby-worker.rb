04fedf6d0b5343f03f85362f8893b281594589dc
Nicholas Long <nicholas.long@nrel.gov>
2013-09-09 17:55:47 -0600

    breaking out ruby for server and worker

---
+++ vagrant/chef/roles/ruby-worker.rb
@@ -0,0 +1,38 @@
+name "ruby"
+description "The bare essentials for servers that are using ruby."
+
+run_list([
+             #"recipe[build-essential]",
+             "recipe[ruby_build]",
+             "recipe[rbenv::system]",
+         #"recipe[rbenv::vagrant]",
+         ])
+
+default_attributes({
+                       :rbenv => {
+                           :upgrade => true,
+                           :rubies => [
+                               {
+                                   :name => '2.0.0-p195',
+                                   :environment => {
+                                       'RUBY_CONFIGURE_OPTS' => '--enable-shared', # needs to be set for openstudio linking
+                                       'CONFIGURE_OPTS' => '--disable-install-doc'
+                                   }
+                               }
+                           ],
+                           :no_rdoc_ri => true,
+                           :global => "2.0.0-p195",
+                           :gems => {
+                               "2.0.0-p195" => [
+                                   {
+                                       :name => "rubygems-bundler",
+                                       :version => "1.2.2",
+                                   },
+                                   {   :name => "mongoid" }
+
+
+                               ]
+                           }
+                       }
+                   }
+)
