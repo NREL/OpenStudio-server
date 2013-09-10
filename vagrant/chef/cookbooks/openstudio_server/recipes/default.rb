d623263bdd8a49bd989b3734038af400bb5f3c03
Nicholas Long <nicholas.long@nrel.gov>
2013-09-09 21:56:59 -0600

    removing packages on worker

--- vagrant/chef/cookbooks/openstudio_server/recipes/default.rb
+++ vagrant/chef/cookbooks/openstudio_server/recipes/default.rb
@@ -3,7 +3,6 @@
 # Recipe:: default
 #
 
-#include_recipe "rails"
 include_recipe "passenger_apache2"
 
 # execute bundle install in directory
@@ -14,24 +13,21 @@ bash "bundle install" do
   EOH
 end
 
-
-# load the test data (eventaully make this a separate recipe)
+# load the test data (eventaully make this a separate recipe - or just remove)
 bash "load default data" do
   code <<-EOH
     cd #{node[:openstudio_server][:server_path]}
-    rake db:seed
+    bundle exec rake db:seed
   EOH
 end
 
 web_app "openstudio-server" do
   docroot "#{node[:openstudio_server][:server_path]}/public"
   server_name "openstudio-server"
-  #
   rails_env "development"
 end
 
 # restart apache?
-
 
 # restart (or start) delayed_job
 bash "restart delayed job" do
