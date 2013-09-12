0d46de4ead9d542e6ffd6b79ad7d183d674fbd50
brian ball <brian.ball@nrel.gov>
2013-09-12 17:12:50 -0600

    fixing errors to get SDP to run on EC2

--- launch-instance/slave_script.sh
+++ launch-instance/slave_script.sh
@@ -2,8 +2,8 @@
 mkdir /home/ubuntu/test
 
 # Change Host
-#echo 54.242.71.153 MASTER_DNS master_name >> /etc/hosts
-echo 54.242.71.153 master_name >> /etc/hosts
+#echo 54.211.125.0 MASTER_DNS master_name >> /etc/hosts
+echo 54.211.125.0 master_name >> /etc/hosts
 
 # Replace Hostname 
 #echo "slave1" > /etc/hostname
