a7befcdde70a4e796611d3d2e026c355858ff3f7
brian ball <brian.ball@nrel.gov>
2013-09-12 14:28:57 -0600

    ruby path issues on dev server for OS

--- vagrant/chef/roles/openstudio-server.rb
+++ vagrant/chef/roles/openstudio-server.rb
@@ -18,7 +18,7 @@ run_list([
 
 default_attributes(
     :openstudio => {
-        :version => "1.0.5.e1d7d0605e",
+        :version => "1.0.5.e1d7d0605f",
         #:checksum => "9180659c77a7fc710cb9826d40ae67c65db0d26bb4bce1a93b64d7e63f4a1f2c"
     },
     :energyplus => {
