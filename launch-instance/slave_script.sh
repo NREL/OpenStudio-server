f87823adf4f920b7113c848fefa216c613a868fb
brian ball <brian.ball@nrel.gov>
2013-09-13 09:41:17 -0600

    testing different instances

--- launch-instance/slave_script.sh
+++ launch-instance/slave_script.sh
@@ -2,8 +2,8 @@
 mkdir /home/ubuntu/test
 
 # Change Host
-#echo 54.211.200.156 MASTER_DNS master_name >> /etc/hosts
-echo 54.211.200.156 master_name >> /etc/hosts
+#echo 54.234.55.95 MASTER_DNS master_name >> /etc/hosts
+echo 54.234.55.95 master_name >> /etc/hosts
 
 # Replace Hostname 
 #echo "slave1" > /etc/hostname
