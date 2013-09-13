d960e75a806c12f8ec10acf0eb2bb7c15e35ce6a
brian ball <brian.ball@nrel.gov>
2013-09-13 15:23:57 -0600

    typo mtn

--- launch-instance/SDP_EC2.rb
+++ launch-instance/SDP_EC2.rb
@@ -53,7 +53,7 @@ puts @r.converse('flag["run"]')
        flag <- dbGetQuery(mongo,"control",'{"_id":1}')
        if (flag["run"] == "FALSE" ){stop(options("show.error.messages"="TRUE"),"run flag is not TRUE")}
        dbDisconnect(mongo)
-       y <- paste("/usr/local/rbenv/shims/ruby -I/usr/local/lib/ruby/site_ruby/2.0.0/ /mtn/openstudio/SimulateDataPoint.rb -d /mtn/openstudio/analysis/data_point_",x," -r AWS",sep="")
+       y <- paste("/usr/local/rbenv/shims/ruby -I/usr/local/lib/ruby/site_ruby/2.0.0/ /mnt/openstudio/SimulateDataPoint.rb -d /mnt/openstudio/analysis/data_point_",x," -r AWS",sep="")
        z <- system(y,intern=TRUE)
        j <- length(z)
        z}
