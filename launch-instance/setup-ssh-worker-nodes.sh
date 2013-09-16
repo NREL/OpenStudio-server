71c4292df05595a39cbf8837c95e83235342a0e1
Nicholas Long <nicholas.long@nrel.gov>
2013-09-16 12:37:25 -0600

    add scripts to configure vagrant boxes for testing

--- launch-instance/setup-ssh-worker-nodes.sh
+++ launch-instance/setup-ssh-worker-nodes.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Read in the ip address file to allow for testing with other files
+IP_FILE=$1
+
 echo "reading in ip_addresses from file"
 while read line
 do
@@ -8,4 +11,4 @@ do
   username=`echo $line | awk -F'|' '{print $2}'`
   password=`echo $line | awk -F'|' '{print $3}'`
   ./setup-ssh-worker-nodes.expect $ipaddress $username $password
-done < "ip_addresses"
+done < ${IP_FILE}
