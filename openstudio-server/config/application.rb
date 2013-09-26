e2657a9ec00771c231c70287fbea0ca6fc628770
Nicholas Long <nicholas.long@nrel.gov>
2013-09-25 22:14:18 -0600

    moving analyses to lib folder that are queued up

--- openstudio-server/config/application.rb
+++ openstudio-server/config/application.rb
@@ -23,7 +23,11 @@ module OpenstudioServer
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib/delayed_job)
+    config.autoload_paths += %W(
+      #{config.root}/lib
+      #{config.root}/lib/delayed_job
+    )
+
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
