b317298619574c388232a951ca90676c62bf5d58
Nicholas Long <nicholas.long@nrel.gov>
2013-09-25 23:49:53 -0600

    flatten results and put into mongo

--- openstudio-server/lib/tasks/run.rake
+++ openstudio-server/lib/tasks/run.rake
@@ -6,4 +6,13 @@ namespace :run do
 
     a.start_r_and_run_sample
   end
+
+  desc 'test pulling in os data'
+  task :os_data => :environment do
+    dp = DataPoint.first
+    puts dp.id
+    dp.save_results_from_openstudio_json
+
+
+  end
 end
--- prototype/pat/CommunicateResults_Mongo.rb
+++ prototype/pat/CommunicateResults_Mongo.rb
@@ -9,40 +9,40 @@ def communicateStarted(id)
   dp = DataPoint.find_or_create_by(uuid: id)
   dp.status = "started"
   dp.ip_address = Socket.gethostname
-  dp.save!  
+  dp.save!
 end
 
 
-def getJSON(id,directory)
+def getJSON(id, directory)
 
   result = [] # [data_point_json, analysis_json]
-  
+
   project_path = directory.parent_path.parent_path
-  
+
   dp = DataPoint.find_or_create_by(uuid: id)
   data_point_hash = Hash.new
   data_point_hash[:data_point] = dp
   data_point_hash[:metadata] = dp[:os_metadata]
   result[0] = data_point_hash.to_json
-  
+
   # DLM: temp debugging code
   #data_point_json_path = directory / OpenStudio::Path.new("data_point_in.json")
   #File.open(data_point_json_path.to_s, 'w') do |f|
   #  f.puts result[0]
   #end
-  
+
   analysis = dp.analysis
   analysis_hash = Hash.new
   analysis_hash[:analysis] = analysis
   analysis_hash[:metadata] = analysis[:os_metadata]
   result[1] = analysis_hash.to_json
-  
+
   # DLM: temp debugging code
   #formulation_json_path = directory / OpenStudio::Path.new("formulation.json")
   #File.open(formulation_json_path.to_s, 'w') do |f|
   #  f.puts result[1]
   #end
-  
+
   return result
 end
 
@@ -52,29 +52,27 @@ def communicateDatapoint(data_point)
   dp = DataPoint.find_or_create_by(uuid: id)
   id = OpenStudio::removeBraces(data_point.analysisUUID.get)
   dp.analysis = Analysis.find_or_create_by(uuid: id)
-  dp.values = data_point.variableValues.map{|v| v.toDouble}
-  dp.save!  
+  dp.values = data_point.variableValues.map { |v| v.toDouble }
+  dp.save!
 end
 
 
-def communicateResults(data_point,directory)  
+def communicateResults(data_point, directory)
   id = OpenStudio::removeBraces(data_point.uuid)
 
   # create zip file
   zipFilePath = directory / OpenStudio::Path.new("data_point_" + id + ".zip")
-  zipFile = OpenStudio::ZipFile.new(zipFilePath,false)
-  zipFile.addFile(directory / OpenStudio::Path.new("openstudio.log"),
-                  OpenStudio::Path.new("openstudio.log"))
-  zipFile.addFile(directory / OpenStudio::Path.new("run.db"),
-                  OpenStudio::Path.new("run.db"))
+  zipFile = OpenStudio::ZipFile.new(zipFilePath, false)
+  zipFile.addFile(directory / OpenStudio::Path.new("openstudio.log"), OpenStudio::Path.new("openstudio.log"))
+  zipFile.addFile(directory / OpenStudio::Path.new("run.db"), OpenStudio::Path.new("run.db"))
   Dir.foreach(directory.to_s) do |item|
     next if item == '.' or item == '..'
     fullPath = directory / OpenStudio::Path.new(item)
     if File.directory?(fullPath.to_s)
-      zipFile.addDirectory(fullPath,OpenStudio::Path.new(item))
-    end    
+      zipFile.addDirectory(fullPath, OpenStudio::Path.new(item))
+    end
   end
-  
+
   # let mongo know that the data point is complete
   dp = DataPoint.find_or_create_by(uuid: id)
   data_point_options = OpenStudio::Analysis::DataPointSerializationOptions.new(directory.parent_path)
@@ -87,5 +85,5 @@ end
 def communicateFailure(id)
   dp = DataPoint.find_or_create_by(uuid: id)
   dp.status = "completed"
-  dp.save!  
+  dp.save!
 end
