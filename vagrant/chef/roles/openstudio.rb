39484feb546000bcb07551988da5763be954ac2b
Nicholas Long <nicholas.long@nrel.gov>
2013-10-07 10:56:09 -0600

    update openstudio cookbook. make role for openstudio

---
+++ vagrant/chef/roles/openstudio.rb
@@ -0,0 +1,21 @@
+name "openstudio"
+description "Install and configure OpenStudio and EnergyPlus"
+
+run_list([
+             # Default iptables setup on all servers.
+             "recipe[openstudio]",
+             "recipe[energyplus]",
+         ])
+
+
+default_attributes(
+    :openstudio => {
+        :version => "1.1.0",
+        :version_revision => "06c98f5a6f"
+        #:checksum => "9180659c77a7fc710cb9826d40ae67c65db0d26bb4bce1a93b64d7e63f4a1f2c"
+    },
+    :energyplus => {
+        :version => "800008",
+        #:checksum => "c1ec1499f964bad8638d3c732c9bd10793dd4052a188cd06bb49288d3d962e09"
+    }
+)
