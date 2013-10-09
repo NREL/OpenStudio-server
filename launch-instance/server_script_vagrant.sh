8ec198f759eb1b7fccef1b6fc8fef071bb15b798
Nicholas Long <nicholas.long@nrel.gov>
2013-10-08 21:52:04 -0600

    major reorg of files

--- launch-instance/server_script_vagrant.sh
+++ launch-instance/server_script_vagrant.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Change Host File Entries
-ENTRY="MASTER_IP MASTER_HOSTNAME MASTER_ALIAS"
+ENTRY="localhost localhost master"
 FILE=/etc/hosts
 if grep -q "$ENTRY" $FILE; then
   echo "entry already exists"
@@ -12,18 +12,20 @@ fi
 # copy all the setup scripts to the appropriate home directory
 cp /data/launch-instance/setup* ~
 chmod 775 ~/setup*
+#chown ubuntu:ubuntu /home/ubuntu/setup*
 
 # Force the generation of various directories that are in the EBS mnt
 sudo rm -rf /mnt/openstudio
 sudo mkdir -p /mnt/openstudio
-sudo chmod 777 /mnt/openstudio
+sudo chmod 777 /var/www/rails/openstudio/public
 
 # save some files into the right directory
-cp /data/prototype/pat/SimulateDataPoint.rb /mnt/openstudio/
-cp /data/prototype/pat/CommunicateResults_Mongo.rb /mnt/openstudio/
+cp -rf /data/worker-nodes/* /mnt/openstudio/
 
 # copy over the models needed for mongo
-mkdir -p /mnt/openstudio/rails-models
-cp /data/prototype/pat/rails-models.zip /mnt/openstudio/rails-models/
-cd /mnt/openstudio/rails-models
-unzip -o rails-models.zip
+cd /mnt/openstudio/rails-models && unzip -o rails-models.zip
+
+sudo chmod -R 777 /mnt/openstudio
+
+#file flag the user_data has completed
+cat /dev/null > ~/user_data_done
