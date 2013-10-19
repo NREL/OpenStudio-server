9c5285d5c50f331463ad6f85f724632e1671893e
Nicholas Long <nicholas.long@nrel.gov>
2013-10-19 15:21:45 -0600

    oops. remove other mongodb recipe

--- vagrant/chef/roles/openstudio-worker.rb
+++ vagrant/chef/roles/openstudio-worker.rb
@@ -4,11 +4,10 @@ description "Install and configure OpenStudio for use with Ruby on Rails"
 run_list([
              # Default iptables setup on all servers.
              "role[base]",
-             "role[mongodb]",
              "role[ruby-worker]",
+             "role[mongodb]",
              #"role[web_base]",
              "role[r-project]",
-             "recipe[mongodb::server]",
              "role[openstudio]",
          ])
 
