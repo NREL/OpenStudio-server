8fba8af81009c7df0b4cbc1a4c904992961c5bc2
Nicholas Long <nicholas.long@nrel.gov>
2013-10-24 14:49:41 -0600

    enable server to run simulations

--- launch-instance/configure_vagrant_server.sh
+++ launch-instance/configure_vagrant_server.sh
@@ -7,9 +7,9 @@ cd /data/launch-instance && ./setup-ssh-keys-vagrant.expect
 cp -f /data/launch-instance/ip_addresses_vagrant.template /data/launch-instance/ip_addresses
 cd /data/launch-instance && ./setup-ssh-worker-nodes.sh ip_addresses
 
-# need to setup the hosts file
+# need to setup the hosts file and the local paths for writing results
 sudo /data/launch-instance/server_script_vagrant.sh
 
-# make sure that the openstudio directory for simulations exists and is writable
-sudo mkdir -p /mnt/openstudio
-sudo chmod 777 /mnt/openstudio
+# rename the mongoid-vagrant template to mongoid.yml
+mv /mnt/openstudio/rails-models/mongoid-vagrant.yml /mnt/openstudio/rails-models/mongoid.yml
+
