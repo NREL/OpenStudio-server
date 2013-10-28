959a76145a06f4ecc397f735dde212ce79aa0820
brian ball <brian.ball@nrel.gov>
2013-10-28 08:15:21 -0600

    cleanup script to reduce vagrant box size

---
+++ launch-instance/setup-cleanup.sh
@@ -0,0 +1,141 @@
+#!/bin/sh
+#sudo apt-get remove --auto-remove texlive* -y
+#sudo apt-get purge --auto-remove texlive* -y
+#sudo apt-get remove --auto-remove chef -y
+#sudo rm -rf /opt/
+#sudo apt-get purge --auto-remove chef -y
+sudo apt-get remove gfortran -y
+
+sudo apt-get remove --auto-remove gir1.2-freedesktop -y
+sudo apt-get remove --auto-remove gir1.2-pango-1.0 -y
+sudo apt-get remove --auto-remove libcairo-script-interpreter2 -y
+sudo apt-get remove --auto-remove libcairo2-dev -y
+sudo apt-get remove --auto-remove libfontconfig1-dev -y
+sudo apt-get remove --auto-remove libfreetype6-dev -y
+sudo apt-get remove --auto-remove libglib2.0-bin -y
+sudo apt-get remove --auto-remove libglib2.0-data -y
+sudo apt-get remove --auto-remove libglib2.0-dev -y
+sudo apt-get remove --auto-remove libkpathsea5 -y
+sudo apt-get remove --auto-remove liblzma-dev -y
+sudo apt-get remove --auto-remove libpango1.0-dev -y
+sudo apt-get remove --auto-remove libpixman-1-dev -y
+sudo apt-get remove --auto-remove libpoppler19 -y
+sudo apt-get remove --auto-remove libtiff4-dev -y
+sudo apt-get remove --auto-remove libtiffxx0c2 -y
+sudo apt-get remove --auto-remove libxcb-render0-dev -y
+sudo apt-get remove --auto-remove libxcb-shm0-dev -y
+sudo apt-get remove --auto-remove libxext-dev -y
+sudo apt-get remove --auto-remove libxfont1 -y
+sudo apt-get remove --auto-remove libxft-dev -y
+sudo apt-get remove --auto-remove libxkbfile1 -y
+sudo apt-get remove --auto-remove libxrender-dev -y
+sudo apt-get remove --auto-remove libxss-dev -y
+sudo apt-get remove --auto-remove luatex -y
+sudo apt-get remove --auto-remove openjdk-6-jdk -y
+sudo apt-get remove --auto-remove openjdk-6-jre -y
+sudo apt-get remove --auto-remove openjdk-6-jre-headless -y
+sudo apt-get remove --auto-remove openjdk-6-jre-lib -y
+sudo apt-get remove --auto-remove preview-latex-style -y
+sudo apt-get remove --auto-remove tcl8.5-dev -y
+sudo apt-get remove --auto-remove tex-common -y
+sudo apt-get remove --auto-remove texi2html -y
+sudo apt-get remove --auto-remove texinfo -y
+sudo apt-get remove --auto-remove texlive-base -y
+sudo apt-get remove --auto-remove texlive-binaries -y
+sudo apt-get remove --auto-remove texlive-common -y
+sudo apt-get remove --auto-remove texlive-doc-base -y
+sudo apt-get remove --auto-remove texlive-extra-utils -y
+sudo apt-get remove --auto-remove texlive-fonts-extra -y
+sudo apt-get remove --auto-remove texlive-fonts-recommended -y
+sudo apt-get remove --auto-remove texlive-generic-recommended -y
+sudo apt-get remove --auto-remove texlive-latex-base -y
+sudo apt-get remove --auto-remove texlive-latex-extra -y
+sudo apt-get remove --auto-remove texlive-latex-recommended -y
+sudo apt-get remove --auto-remove texlive-pictures -y
+sudo apt-get remove --auto-remove tk8.5-dev -y
+sudo apt-get remove --auto-remove x11-xkb-utils -y
+sudo apt-get remove --auto-remove x11proto-render-dev -y
+sudo apt-get remove --auto-remove x11proto-scrnsaver-dev -y
+sudo apt-get remove --auto-remove x11proto-xext-dev -y
+sudo apt-get remove --auto-remove xfonts-base -y
+sudo apt-get remove --auto-remove xfonts-encodings -y
+sudo apt-get remove --auto-remove xfonts-utils -y
+sudo apt-get remove --auto-remove xserver-common -y
+sudo apt-get remove --auto-remove xvfb -y
+
+sudo apt-get purge --auto-remove gir1.2-freedesktop -y
+sudo apt-get purge --auto-remove gir1.2-pango-1.0 -y
+sudo apt-get purge --auto-remove libcairo-script-interpreter2 -y
+sudo apt-get purge --auto-remove libcairo2-dev -y
+sudo apt-get purge --auto-remove libfontconfig1-dev -y
+sudo apt-get purge --auto-remove libfreetype6-dev -y
+sudo apt-get purge --auto-remove libglib2.0-bin -y
+sudo apt-get purge --auto-remove libglib2.0-data -y
+sudo apt-get purge --auto-remove libglib2.0-dev -y
+sudo apt-get purge --auto-remove libkpathsea5 -y
+sudo apt-get purge --auto-remove liblzma-dev -y
+sudo apt-get purge --auto-remove libpango1.0-dev -y
+sudo apt-get purge --auto-remove libpixman-1-dev -y
+sudo apt-get purge --auto-remove libpoppler19 -y
+sudo apt-get purge --auto-remove libtiff4-dev -y
+sudo apt-get purge --auto-remove libtiffxx0c2 -y
+sudo apt-get purge --auto-remove libxcb-render0-dev -y
+sudo apt-get purge --auto-remove libxcb-shm0-dev -y
+sudo apt-get purge --auto-remove libxext-dev -y
+sudo apt-get purge --auto-remove libxfont1 -y
+sudo apt-get purge --auto-remove libxft-dev -y
+sudo apt-get purge --auto-remove libxkbfile1 -y
+sudo apt-get purge --auto-remove libxrender-dev -y
+sudo apt-get purge --auto-remove libxss-dev -y
+sudo apt-get purge --auto-remove luatex -y
+sudo apt-get purge --auto-remove openjdk-6-jdk -y
+sudo apt-get purge --auto-remove openjdk-6-jre -y
+sudo apt-get purge --auto-remove openjdk-6-jre-headless -y
+sudo apt-get purge --auto-remove openjdk-6-jre-lib -y
+sudo apt-get purge --auto-remove preview-latex-style -y
+sudo apt-get purge --auto-remove tcl8.5-dev -y
+sudo apt-get purge --auto-remove tex-common -y
+sudo apt-get purge --auto-remove texi2html -y
+sudo apt-get purge --auto-remove texinfo -y
+sudo apt-get purge --auto-remove texlive-base -y
+sudo apt-get purge --auto-remove texlive-binaries -y
+sudo apt-get purge --auto-remove texlive-common -y
+sudo apt-get purge --auto-remove texlive-doc-base -y
+sudo apt-get purge --auto-remove texlive-extra-utils -y
+sudo apt-get purge --auto-remove texlive-fonts-extra -y
+sudo apt-get purge --auto-remove texlive-fonts-recommended -y
+sudo apt-get purge --auto-remove texlive-generic-recommended -y
+sudo apt-get purge --auto-remove texlive-latex-base -y
+sudo apt-get purge --auto-remove texlive-latex-extra -y
+sudo apt-get purge --auto-remove texlive-latex-recommended -y
+sudo apt-get purge --auto-remove texlive-pictures -y
+sudo apt-get purge --auto-remove tk8.5-dev -y
+sudo apt-get purge --auto-remove x11-xkb-utils -y
+sudo apt-get purge --auto-remove x11proto-render-dev -y
+sudo apt-get purge --auto-remove x11proto-scrnsaver-dev -y
+sudo apt-get purge --auto-remove x11proto-xext-dev -y
+sudo apt-get purge --auto-remove xfonts-base -y
+sudo apt-get purge --auto-remove xfonts-encodings -y
+sudo apt-get purge --auto-remove xfonts-utils -y
+sudo apt-get purge --auto-remove xserver-common -y
+sudo apt-get purge --auto-remove xvfb -y
+
+sudo apt-get autoclean
+sudo apt-get clean
+
+sudo rm -rf /usr/local/lib/R-3.0.2.tar.gz
+sudo rm -rf /tmp/*.tar.gz
+sudo rm /var/cache/apt/archives/*.deb
+
+
+sudo dd if=/dev/zero of=/EMPTY bs=1M
+sudo rm /EMPTY
+#sudo dd if=/dev/zero of=/home/EMPTY bs=1M
+#sudo rm /home/EMPTY
+#sudo dd if=/dev/zero of=/usr/EMPTY bs=1M
+#sudo rm /usr/EMPTY
+#sudo dd if=/dev/zero of=/var/EMPTY bs=1M
+#sudo rm /var/EMPTY
+#sudo dd if=/dev/zero of=/tmp/EMPTY bs=1M
+#sudo rm /tmp/EMPTY
+#exit
