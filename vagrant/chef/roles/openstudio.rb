8bcfc95f4adc55d2ac2ee3af94ad5861e672ca62
Elaine Hale <elaine.hale@nrel.gov>
2013-11-14 14:50:25 -0700

    proper OS build for this branch.

--- vagrant/chef/roles/openstudio.rb
+++ vagrant/chef/roles/openstudio.rb
@@ -10,8 +10,8 @@ run_list([
 
 default_attributes(
     :openstudio => {
-        :version => "1.1.1",
-        :version_revision => "07dc4f3e62",
+        :version => "1.1.2",
+        :version_revision => "3f6008203d",
         :platform => "Linux-Ruby2.0"
         #:checksum => "9180659c77a7fc710cb9826d40ae67c65db0d26bb4bce1a93b64d7e63f4a1f2c"
     },
