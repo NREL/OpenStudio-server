c130adf3d44161e37e700b4317b20c5fb719acc7
Nicholas Long <nicholas.long@nrel.gov>
2013-11-17 17:34:50 -0700

    use openstudio-aws gem for setting up the cluster now

--- launch-instance/runEC2.rb
+++ launch-instance/runEC2.rb
@@ -2,128 +2,32 @@
 # on EC2. After the servers are configures, the latter part of the script uses the API to
 # run an example analysis.
 
-require "./AwsConfig.rb"
-require "json"
-
-# use the aws class that lives in the OpenStudio Repository now.  Make sure to update the PATH below to
-# whereever you OpenStudio checkout is (currently on the AWSProvider branch in OS)
-#OS_PATH = "C:/Projects/OpenStudio"
-OS_PATH = "/Users/nlong/Working/OpenStudio"
-#OS_PATH = "C:/working/openstudio"
-
-RUBY_LOC = "ruby"
-#RUBY_LOC = "c:/ruby-2.0.0-p247-i386-mingw32/bin/ruby.exe"
+# Load the gems from your bundle (do bundle install if you haven't already)
+require 'rubygems'
+require 'bundler/setup'
+                              
+require 'openstudio-aws'
 
 # Global Options
-CREATE_SERVER=true
-CREATE_WORKER=true
 WORKER_INSTANCES=2
-TEST_SSH=true
 
-# read in the config.yml file to get the secret/private key
-config = AwsConfig.new()
-a = Time.now
-# Launch the master
-if CREATE_SERVER
-  instance_data = {instance_type: "m2.xlarge" }
-  #instance_data = {instance_type: "t1.micro" }
-  instance_string = instance_data.to_json.gsub("\"", "\\\\\"")
+#require 'openstudio-aws'
+aws = OpenStudio::Aws::Aws.new()
+#server_options = {instance_type: "t1.micro" }
+server_options = {instance_type: "m1.small" }
+#server_options = {instance_type: "m2.xlarge" }
 
-  start_string = "#{RUBY_LOC} #{OS_PATH}/openstudiocore/ruby/cloud/aws.rb.in #{config.access_key} #{config.secret_key} us-east-1 EC2 launch_server \"#{instance_string}\""
-  puts "#{start_string}"
-  server_data_str = `#{start_string}`
-  puts server_data_str
-  server_data = JSON.parse(server_data_str, :symbolize_names => true)
+worker_options = {instance_type: "m1.small" }
+#worker_options = {instance_type: "m2.xlarge" }
+#worker_options = {instance_type: "m2.2xlarge" }
+#worker_options = {instance_type: "m2.4xlarge" }
+#worker_options = {instance_type: "cc2.8xlarge" }
 
-  # Save off the private key for later access
-  server_key_file = "ec2_server_key.pem"
-  File.open(server_key_file, "w") { |f| f << server_data[:private_key] }
-  File.chmod(0600, server_key_file)
+# Create the server
+aws.create_server(server_options)
 
-  # Save off the server data to be loaded into the worker nodes.  The Private key needs to e read from a
-  # file in the worker node, so save that name instead in the HASH along with a couple other changes
-  server_data[:private_key] = server_key_file
-  server_data[:server_id] = server_data[:server][:id]
-  server_data[:server_ip] = server_data[:server][:ip]
-  server_data[:server_dns] = server_data[:server][:dns]
-  File.open("server_data.json", "w") { |f| f << JSON.pretty_generate(server_data) }
+# Create the worker
+aws.create_workers(WORKER_INSTANCES, worker_options)
 
-  # Print out some debugging commands (probably work on mac/linux only)
-  puts ""
-  puts "Server SSH Command:"
-  puts "ssh -i #{server_key_file} ubuntu@#{server_data[:server][:dns]}"
-end
-
-# Launch the workers
-if CREATE_WORKER
-  server_json = JSON.parse(File.read("server_data.json"), :symbolize_names => true)
-
-  # How many instances?
-  server_json[:instance_type] = "cc2.8xlarge"
-  #server_json[:instance_type] = "m2.4xlarge"
-  #server_json[:instance_type] = "m2.2xlarge"
-  #server_json[:instance_type] = "t1.micro"
-  server_json[:num] = WORKER_INSTANCES
-  server_string = server_json.to_json.gsub("\"", "\\\\\"")
-
-  start_string = "#{RUBY_LOC} #{OS_PATH}/openstudiocore/ruby/cloud/aws.rb.in #{config.access_key} #{config.secret_key} us-east-1 EC2 launch_workers \"#{server_string}\""
-  worker_data_string = `#{start_string}`
-  worker_data = JSON.parse(worker_data_string, :symbolize_names => true)
-  File.open("worker_data.json", "w") { |f| f << JSON.pretty_generate(worker_data) }
-
-  # Print out some debugging commands (probably work on mac/linux only)
-  worker_data[:workers].each do |worker|
-    puts ""
-    puts "Worker SSH Command:"
-    puts "ssh -i #{server_json[:private_key]} ubuntu@#{worker[:dns]}"
-  end
-end
-
-### Debugging section
-
-
-if TEST_SSH
-  # This is taken out of the OpenStudio AWS config file to do some testing
-  def send_command(host, command, key)
-    require 'net/http'
-    require 'net/scp'
-    require 'net/ssh'
-
-    retries = 0
-    begin
-      puts "connecting.."
-      output = ''
-      Net::SSH.start(host, 'ubuntu', :key_data => [key]) do |ssh|
-        response = ssh.exec!(command)
-        output += response if !response.nil?
-      end
-      return output
-    rescue Net::SSH::HostKeyMismatch => e
-      e.remember_host!
-      # key mismatch, retry
-      return if retries == 2
-      retries += 1
-      sleep 1
-      retry
-    rescue Net::SSH::AuthenticationFailed
-      error(-1, "Incorrect private key")
-    rescue SystemCallError, Timeout::Error => e
-      # port 22 might not be available immediately after the instance finishes launching
-      return if retries == 2
-      retries += 1
-      sleep 1
-      retry
-    rescue Exception => e
-      puts e.message
-      puts e.backtrace.inspect
-    end
-  end
-
-  server_json = JSON.parse(File.read("server_data.json"), :symbolize_names => true)
-  
-  b = Time.now
-  delta = b.to_f - a.to_f
-  puts "startup time is #{delta}"
-  #puts send_command(server_json[:server_ip], 'nproc | tr -d "\n"', File.read("ec2_server_key.pem"))
-end
+# At this point the instances are up-and-running. To kill you must go to AWS console and kill the instances manually
 
