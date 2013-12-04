ab30f44cda193e16f68b49cc833c0cec03eec06b
Brian Ball <Brian.Ball@nrel.gov>
2013-12-04 11:02:31 -0700

    new version including SPEA2

--- prototyping/NRELmoo/R/plot_moo.R
+++ prototyping/NRELmoo/R/plot_moo.R
@@ -1,20 +1,31 @@
 
-plot_objectives <- function(objectives_save, objDim, gen){
-   objectives_plot <- objectives_save[,1:(objDim)]
-   plot(objectives_plot, xlim=c(min(objectives_save[,1]),max(objectives_save[,1])), ylim=c(min(objectives_save[,2]),max(objectives_save[,2])))
-   
-   for (i in 2:(gen+1)){
-     objectives_plot <- objectives_save[,((i-1)*objDim+1):(objDim*i)]
-     points(objectives_plot)
-     Sys.sleep(1.0)
-   }
+plot_objectives <- function(objectives_save, objDim){
+    objectives_plot <- objectives_save[,1:(objDim)]
+    plot(objectives_plot, xlim=c(min(na.omit(objectives_save[,1])),max(na.omit(objectives_save[,1]))), ylim=c(min(na.omit(objectives_save[,2])),max(na.omit(objectives_save[,2]))))
+    
+    if (ncol(objectives_save)%%objDim != 0) {
+      print("save object not multiple of objective dimension")
+      stop}
+      
+    plot_length <- ncol(objectives_save)%/%objDim
+    for (i in 2:(plot_length)){
+        objectives_plot <- objectives_save[,((i-1)*objDim+1):(objDim*i)]
+        points(objectives_plot)
+        Sys.sleep(1.0)
+    }
 }
    
-plot_parameters <- function(parameters_save, noVars, gen){
+plot_parameters <- function(parameters_save, noVars){
    parameters_plot <- parameters_save[,1:(noVars)]
-   plot(parameters_plot, xlim=c(min(parameters_save[,1]),max(parameters_save[,1])), ylim=c(min(parameters_save[,2]),max(parameters_save[,2])))
-
-   for (i in 2:(gen+1)){
+   plot(parameters_plot, xlim=c(min(na.omit(parameters_save[,1])),max(na.omit(parameters_save[,1]))), ylim=c(min(na.omit(parameters_save[,2])),max(na.omit(parameters_save[,2]))))
+    
+   if (ncol(objectives_save)%%noVars != 0) {
+      print("save object not multiple of number of variables")
+      stop}
+      
+   plot_length <- ncol(objectives_save)%/%noVars
+    
+   for (i in 2:(plot_length)){
      parameters_plot <- parameters_save[,((i-1)*noVars+1):(noVars*i)]
      points(parameters_plot)
      Sys.sleep(1.0)
