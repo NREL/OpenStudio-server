981dd416c91e63ef8a7b6521f254be1877373e1c
Brian Ball <Brian.Ball@nrel.gov>
2013-12-16 17:08:42 -0700

    R CMD build/check passes

--- lib/R-packages-code/NRELsnowFT/tests/run_tests.R
+++
@@ -1,8 +0,0 @@
-#source('/Users/hana/myprojects/snowFT/tests/test_functions.R')
-source('test_functions.R')
-run.rnorm.sock()
-run.rnorm.seq()
-#run.rnorm.sock.cluster.args()
-#run.rnorm.pvm()
-#run.rnorm.mpi()
-check.reproducibility.for.seq.and.par()
