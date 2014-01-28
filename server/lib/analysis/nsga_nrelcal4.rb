21ea4d1eb7237723dd7314640f294292a3c958f5
Brian Ball <Brian.Ball@nrel.gov>
2014-01-28 16:00:45 -0700

    removing hard-coded objective functions on algorithm side

--- server/lib/analysis/nsga_nrelcal4.rb
+++ server/lib/analysis/nsga_nrelcal4.rb
@@ -240,15 +240,24 @@ class Analysis::NsgaNrelcal4
 
               # read in the results from the objective function file
               # TODO: verify that the file exists
-              # TODO: determine how to handle if the objective function value = nil/null    
+              # TODO: determine how to handle if the objective function value = nil/null 
+              #       Right now it sets everything to 0.0
               object_file <- paste(data_point_directory,"/objectives.json",sep="")
               json <- fromJSON(file=object_file)
               obj <- NULL
               for (i in 1:objDim){
                 objfuntemp <- paste("objective_function_",i,sep="")
-                objtemp <- as.numeric(json[objfuntemp])
+                if (json[objfuntemp] != "nil"){
+                  objtemp <- as.numeric(json[objfuntemp])
+                } else {
+                  objtemp <- 0.0
+                }
                 objfuntargtemp <- paste("objective_function_target_",i,sep="")
-                objtemp2 <- as.numeric(json[objfuntargtemp])
+                if (json[objfuntargtemp] != "nil"){
+                  objtemp2 <- as.numeric(json[objfuntargtemp])
+                } else {
+                  objtemp2 <- 0.0
+                }
                 obj[i] <- abs(objtemp - objtemp2)
               }
               print(paste("Objective function results are:",obj))   
