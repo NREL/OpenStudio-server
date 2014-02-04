09df9bdedf8f8d8ed9a3323e35485a915d3404ef
Nicholas Long <nicholas.long@nrel.gov>
2014-02-04 09:32:36 -0700

    reorg test to run LHS first

--- testing/run_tests.rb
+++ testing/run_tests.rb
@@ -6,40 +6,11 @@ require 'openstudio-analysis' # Need to install openstudio-analysis gem
 
 hostname = ARGV[0] || "http://localhost:8080"
 
-#HOSTNAME = "http://ec2-107-20-82-197.compute-1.amazonaws.com"
-
 # Initialize the ServerAPI
 options = {hostname: hostname}
 api = OpenStudio::Analysis::ServerApi.new(options)
 
 api.delete_all()
-
-project_options = {:project_name => "PAT Export"}
-project_id = api.new_project(project_options)
-
-# ===== Disk IO Benchmark - Using PAT JSON Files=====
-formulation_file = "./PATTestExport/analysis.json"
-analysis_zip_file = "./PATTestExport/project.zip"
-datapoint_files = Dir.glob("./PATTestExport/data_points_*.json")
-
-analysis_options = {
-    formulation_file: formulation_file,
-    upload_file: analysis_zip_file,
-    reset_uuids: true,
-    analysis_name: "PAT Export with 8 data points"
-}
-analysis_id = api.new_analysis(project_id, analysis_options)
-
-datapoint_files.each do |dp|
-  datapoint_options = {datapoints_file: dp, reset_uuids: true}
-  api.upload_datapoints(analysis_id, datapoint_options)
-end
-
-run_options = {
-    analysis_action: "start",
-    without_delay: false,
-    analysis_type: 'batch_run'}
-api.run_analysis(analysis_id, run_options)
 
 # ===== LHS Sample and Run =====
 formulation_file = "./SimpleContinuousExample/analysis.json" # fast models (~10 secs) with pivots
@@ -59,7 +30,7 @@ analysis_id = api.new_analysis(project_id, analysis_options)
 
 run_options = {
     analysis_action: "start",
-    without_delay: true,
+    without_delay: false,
     analysis_type: "lhs",
     allow_multiple_jobs: false,
     use_server_as_worker: false,
@@ -84,6 +55,32 @@ run_options = {
 }
 api.run_analysis(analysis_id, run_options)
 
+
+# ===== Disk IO Benchmark - Using PAT JSON Files=====
+project_options = {:project_name => "PAT Export"}
+project_id = api.new_project(project_options)
+formulation_file = "./PATTestExport/analysis.json"
+analysis_zip_file = "./PATTestExport/project.zip"
+datapoint_files = Dir.glob("./PATTestExport/data_points_*.json")
+
+analysis_options = {
+    formulation_file: formulation_file,
+    upload_file: analysis_zip_file,
+    reset_uuids: true,
+    analysis_name: "PAT Export with 8 data points"
+}
+analysis_id = api.new_analysis(project_id, analysis_options)
+
+datapoint_files.each do |dp|
+  datapoint_options = {datapoints_file: dp, reset_uuids: true}
+  api.upload_datapoints(analysis_id, datapoint_options)
+end
+
+run_options = {
+    analysis_action: "start",
+    without_delay: false,
+    analysis_type: 'batch_run'}
+api.run_analysis(analysis_id, run_options)
 
 # === NSGA2 ==
 
