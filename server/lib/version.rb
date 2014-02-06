545236568981e46b22f84ae52f3ef8ea2fe98c24
Alex Swindler <Alex.Swindler@nrel.gov>
2014-02-06 08:30:51 -0700

    Updating to v1.3.11

--- server/lib/version.rb
+++ server/lib/version.rb
@@ -1,4 +1,4 @@
 module OpenstudioServer
-  VERSION = "1.3.10"
+  VERSION = "1.3.11"
   VERSION_EXT = "" # with preceding - or +
 end
--- vagrant/chef/roles/openstudio.rb
+++ vagrant/chef/roles/openstudio.rb
@@ -11,7 +11,7 @@ run_list([
 default_attributes(
     :openstudio => {
         :version => "1.2.3",
-        :version_revision => "02346785ab",
+        :version_revision => "bd1d5f5de9",
         :platform => "Linux-Ruby2.0"
         #:checksum => "9180659c77a7fc710cb9826d40ae67c65db0d26bb4bce1a93b64d7e63f4a1f2c"
     },
