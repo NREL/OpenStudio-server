0a94fe92e05068fae95625868749486d90f68609
Nicholas Long <nicholas.long@nrel.gov>
2014-02-18 10:40:33 -0700

    somehow the old openstudio version is missing

--- vagrant/chef/roles/openstudio.rb
+++ vagrant/chef/roles/openstudio.rb
@@ -11,7 +11,7 @@ run_list([
 default_attributes(
     :openstudio => {
         :version => "1.2.3",
-        :version_revision => "b458b80365",
+        :version_revision => "a1f5365baa",
         :platform => "Linux-Ruby2.0"
         #:checksum => "9180659c77a7fc710cb9826d40ae67c65db0d26bb4bce1a93b64d7e63f4a1f2c"
     },
