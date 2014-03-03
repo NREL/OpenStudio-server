e65aea679d7efcbd218491a6200d9f381bb4ba4b
Alex Swindler <Alex.Swindler@nrel.gov>
2014-03-03 08:22:10 -0700

    Updating to v1.3.17 to test Jason's branch

--- vagrant/chef/roles/openstudio.rb
+++ vagrant/chef/roles/openstudio.rb
@@ -11,7 +11,7 @@ run_list([
 default_attributes(
     :openstudio => {
         :version => "1.2.5",
-        :version_revision => "51597e1653",
+        :version_revision => "72702a015b",
         :platform => "Linux-Ruby2.0"
         #:checksum => "9180659c77a7fc710cb9826d40ae67c65db0d26bb4bce1a93b64d7e63f4a1f2c"
     },
