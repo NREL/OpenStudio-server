d056105ed8f06228dfa4b10284209d587cec9f93
Brian Ball <Brian.Ball@nrel.gov>
2014-03-05 18:50:40 -0700

    updating OS updating post_process update R cookbook to fix path problems

--- worker-nodes/post_process_monthly.rb
+++ worker-nodes/post_process_monthly.rb
@@ -207,7 +207,6 @@ ensure
   paths_to_rm << Pathname.glob("*.osm")
   paths_to_rm << Pathname.glob("*.ini")
   paths_to_rm << Pathname.glob("*.idf")
-  #paths_to_rm << Pathname.glob("*.sql")
   paths_to_rm << Pathname.glob("EnergyPlus")
   paths_to_rm << Pathname.glob("*.epw")
   paths_to_rm << Pathname.glob("*.idd")
@@ -215,5 +214,5 @@ ensure
   paths_to_rm << Pathname.glob("*.bnd")
   paths_to_rm << Pathname.glob("*.mtd")
   paths_to_rm << Pathname.glob("*.rdd")
-  paths_to_rm.each { |p| FileUtils.rm_rf(p) }
+  #paths_to_rm.each { |p| FileUtils.rm_rf(p) }
 end
