87cb1eccc270fa5abc1f397703476bafe85512fe
kflemin <katherine.fleming@nrel.gov>
2014-03-14 16:43:43 -0600

    added about text and started pie dashboard

--- server/app/controllers/pages_controller.rb
+++ server/app/controllers/pages_controller.rb
@@ -2,4 +2,27 @@ class PagesController < ApplicationController
   def about
     # static page
   end
+
+  def dashboard
+    # main dashboard for the site
+
+    @projects = Project.all
+    @analyses = Analysis.all
+    @failed_runs = DataPoint.where(:status_message => 'datapoint failure').count
+    @total_runs = DataPoint.all.count
+    @completed_cnt = DataPoint.where(:status => 'completed').count
+    @failed_perc = (@failed_runs.to_f/@total_runs.to_f * 100).round(0)
+    @completed_perc = (@completed_cnt.to_f/@total_runs.to_f * 100).round(0)
+
+    @analysis = @analyses.first
+    #@completed_runs = @analysis.data_points.where(:status => 'completed', :status_message => 'completed normal').count
+    #@queued_runs = @analysis.data_points.where(:status => 'queued').count
+    #@running_runs = @analysis.data_points.where(:status => 'running').count
+    #@na_runs = @analysis.data_points.where(:status => 'na').count
+    #@failed_runs = @analysis.data_points.where(:status_message => 'datapoint failure').count
+
+    # count each type of simulation
+    @status_cnt = @analysis.data_points.collection.aggregate("$group" => { "_id" => "$status", count: {"$sum" =>  1} })
+    @failed_cnt = @analysis.data_points.where(:status_message => 'datapoint failure').count
+  end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -95,7 +95,7 @@ OpenstudioServer::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'projects#index'
+  root :to => 'pages#dashboard'
 
   # See how all your routes lay out with "rake routes"
 
