66fa41d4c80341b425f23a5b430ee89aec8cf196
Nicholas Long <nicholas.long@nrel.gov>
2014-04-14 21:17:10 -0600

    fix world writable analysis directory

--- launch-instance/configure_vagrant_worker_data.sh
+++ launch-instance/configure_vagrant_worker_data.sh
@@ -1,15 +1,22 @@
-sudo rm -rf /mnt/openstudio/*
-sudo mkdir -p /mnt/openstudio
-sudo chmod -R 777 /mnt/openstudio
+#!/bin/bash
 
-# Copy worker node files to the run directory location
+# Force the generation of various directories that are in the EBS mnt
+sudo rm -rf /mnt/openstudio
+sudo mkdir -p /mnt/openstudio
+sudo chown -R vagrant:www-data /mnt/openstudio
+sudo chmod -R 775 /mnt/openstudio
+
+# save application files into the right directory
 cp -rf /data/worker-nodes/* /mnt/openstudio/
 
-# Unzip the rails-models
+# copy over the models needed for mongo
 cd /mnt/openstudio/rails-models && unzip -o rails-models.zip
 
-# rename the mongoid-vagrant template to mongoid.yml
+# rename the mongoid-vagrant template to mongoid.yml which is unpacked with unzip
 mv /mnt/openstudio/rails-models/mongoid-vagrant.yml /mnt/openstudio/rails-models/mongoid.yml
 
-# Run this once more to make sure all files have world writable permissions (for now)
-sudo chmod -R 777 /mnt/openstudio
+# rerun the permissions after unzipping the files
+sudo chown -R vagrant:www-data /mnt/openstudio 
+sudo find /mnt/openstudio -type d -print0 | xargs -0 chmod 775
+sudo find /mnt/openstudio -type f -print0 | xargs -0 chmod 664
+
