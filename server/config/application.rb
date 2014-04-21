ee2b85ea9612a2ffc68d279441041077b7e2fadb
Nicholas Long <nicholas.long@nrel.gov>
2014-04-21 16:24:00 -0600

    add initial cap files

--- server/config/application.rb
+++ server/config/application.rb
@@ -30,7 +30,6 @@ module OpenstudioServer
       #{config.root}/lib/delayed_job
     )
 
-
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
