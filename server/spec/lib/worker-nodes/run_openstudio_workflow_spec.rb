620656eade28c92f1e5865e4b4b4428c277d08a9
Nicholas Long <nicholas.long@nrel.gov>
2014-04-22 16:00:00 -0600

    update test--not quite there yet

--- server/spec/lib/worker-nodes/run_openstudio_workflow_spec.rb
+++ server/spec/lib/worker-nodes/run_openstudio_workflow_spec.rb
@@ -20,7 +20,6 @@ describe 'RunOpenStudioWorkflow' do
     options =  {:run_data_point_filename => 'run_openstudio_workflow.rb', :uuid => "bad_model"}
     WorkflowHelpers.prepare_run_directory(@library_path, "#{@library_path}/test/#{options[:uuid]}", options)
 
-    # TODO move the command construction to a helper method
     command = "ruby -I#{@library_path} #{@library_path}/test/#{options[:uuid]}/#{options[:run_data_point_filename]} -u #{options[:uuid]} -d #{@library_path}/test"
     result = `#{command}`
     expect(result).to_not be_nil
@@ -32,8 +31,14 @@ describe 'RunOpenStudioWorkflow' do
     options =  {:run_data_point_filename => 'run_openstudio_workflow.rb', :uuid => "example1"}
     WorkflowHelpers.prepare_run_directory(@library_path, "#{@library_path}/test/#{options[:uuid]}", options)
 
-    # TODO move the command construction to a helper method
+    # copy and extract the needed files
+    FileUtils.cp("spec/files/simple_cont_example.zip", "#{@library_path}/test/#{options[:uuid]}")
+    FileUtils.cp("spec/files/simple_cont_example.json", "#{@library_path}/test/#{options[:uuid]}")
+    Dir.chdir("#{@library_path}/test/#{options[:uuid]}")
+    `unzip -o "#{@library_path}/test/#{options[:uuid]}/simple_cont_example.zip"`
+
     command = "ruby -I#{@library_path} #{@library_path}/test/#{options[:uuid]}/#{options[:run_data_point_filename]} -u #{options[:uuid]} -d #{@library_path}/test"
+    puts "running command #{command}"
     result = `#{command}`
     expect(result).to_not be_nil
     result = result.split("\n").last if result
@@ -41,6 +46,6 @@ describe 'RunOpenStudioWorkflow' do
   end
 
   after :all do
-    #FileUtils.rm_rf("#{@library_path}/test")
+    FileUtils.rm_rf("#{@library_path}/test")
   end
 end
