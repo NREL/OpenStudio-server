85d0ad8ea127029d4db0355918ce5758fa177635
Dan Macumber <dan.macumber@gmail.com>
2014-05-12 15:42:08 -0600

    Parsing attributes, still need to associate with measure that created them

--- worker-nodes/run_openstudio_xml.rb
+++ worker-nodes/run_openstudio_xml.rb
@@ -383,12 +383,12 @@ begin
           result.warnings.each { |w| ros.log_message w.logMessage, true }
           result.errors.each { |w| ros.log_message w.logMessage, true }
           result.info.each { |w| ros.log_message w.logMessage, true }
-          begin
-            result.attributes.each { |att| @output_attributes << JSON.parse(OpenStudio::toJSON(att)) }
-          rescue Exception => e
-            log_message = "TODO: #{__FILE__} failed with #{e.message}, #{e.backtrace.join("\n")}"
-            ros.log_message log_message, true
-          end
+        begin
+          # TODO: associate this with the measure that was just run
+          @output_attributes << JSON.parse(OpenStudio::toJSON(result.attributes), symbolize_names: true)
+        rescue Exception => e
+          log_message = "TODO: #{__FILE__} failed with #{e.message}, #{e.backtrace.join("\n")}"
+          ros.log_message log_message, true
         end
       end
     end
@@ -482,6 +482,7 @@ begin
   result.errors.each { |w| ros.log_message w.logMessage, true }
   result.info.each { |w| ros.log_message w.logMessage, true }
 
+  # TODO: associate this with the measure that was just run
   report_json = JSON.parse(OpenStudio.toJSON(result.attributes), symbolize_names: true)
   ros.log_message "JSON file is #{report_json}"
   File.open("#{run_directory}/standard_report.json", 'w') { |f| f << JSON.pretty_generate(report_json) }
