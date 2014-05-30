d7269e4b2a7d4aa71054bf8e171bb3d2e03334f6
Nicholas Long <nicholas.long@nrel.gov>
2014-05-29 23:32:12 -0600

    testing create data point

--- server/lib/analysis/nsga_nrel.rb
+++ server/lib/analysis/nsga_nrel.rb
@@ -201,8 +201,7 @@ class Analysis::NsgaNrel
               vars[,i] <- sort(vars[,i])
             }
             print(paste("vartypes:",vartypes))
-	    print(paste("varnames:",varnames))
-
+	          print(paste("varnames:",varnames))
 
             #f(x) takes a UUID (x) and runs the datapoint
             f <- function(x){
@@ -213,7 +212,7 @@ class Analysis::NsgaNrel
               }
               dbDisconnect(mongo)
 
-              ruby_command <- "/usr/local/rbenv/shims/ruby -W0"
+              ruby_command <- "cd /mnt/openstudio && /usr/local/rbenv/shims/bundle exec ruby"
               if ("#{@analysis.use_shm}" == "true"){
                 y <- paste(ruby_command," /mnt/openstudio/simulate_data_point.rb -a #{@analysis.id} -u ",x," -x #{@options[:run_data_point_filename]} -r AWS --run-shm",sep="")
               } else {
@@ -231,7 +230,7 @@ class Analysis::NsgaNrel
             #           create a UUID for that data_point and put in database
             #           call f(u) where u is UUID of data_point
             g <- function(x){
-              ruby_command <- "/usr/local/rbenv/shims/ruby -W0"
+              ruby_command <- "cd /mnt/openstudio && /usr/local/rbenv/shims/bundle exec ruby"
               # convert the vector to comma separated values
               w = paste(x, collapse=",")
               y <- paste(ruby_command," /mnt/openstudio/#{@options[:create_data_point_filename]} -a #{@analysis.id} -v ",w, sep="")
