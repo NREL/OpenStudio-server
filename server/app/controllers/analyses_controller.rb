f60f609f29b85b3e1e41d4f731c82b5376d97e46
kflemin <katherine.fleming@nrel.gov>
2014-06-05 09:37:10 -0600

    adding collapsible sections

--- server/app/controllers/analyses_controller.rb
+++ server/app/controllers/analyses_controller.rb
@@ -794,7 +794,8 @@ class AnalysesController < ApplicationController
     variables = nil
     plot_data = nil
 
-    var_fields = [:_id, :perturbable, :display_name, :name, :units]
+    var_fields = [:_id, :perturbable, :display_name, :name, :units, :value_type, :data_type]
+    # makes an array of hashes
     variables = Variable.variables(analysis).only(var_fields).as_json(:only => var_fields)
     variables += Variable.pivots(analysis).only(var_fields).as_json(:only => var_fields)
     variables.sort_by! { |v| v['name'] }
@@ -802,15 +803,13 @@ class AnalysesController < ApplicationController
     # Create a map from the _id to the variables machine name
     variable_name_map = Hash[variables.map { |v| [v['_id'], v['name']] }]
 
-    # initialize the plot fields that will need to be reported
-    plot_fields = [:set_variable_values, :name]
 
+    # flatten all the visualization variables to a queryable syntax
     visualizes = Variable.visualizes(analysis).only(var_fields).as_json(:only => var_fields)
-
-    # flatten all the visualization variables to a queryable syntx
     visualize_map = visualizes.map { |v| "results.#{v['name']}" }
-    plot_fields += [:_id, visualize_map]
-    plot_fields.flatten!
+
+    # initialize the plot fields that will need to be reported
+    plot_fields = [:set_variable_values, :name, :_id] + visualize_map
 
     # Can't call the as_json(:only) method on this probably because of the nested results hash
     plot_data = analysis.data_points.where(status: 'completed').order_by(:created_at.asc).only(plot_fields).as_json
