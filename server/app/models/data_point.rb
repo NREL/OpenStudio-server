b7c82a56671b0453375920e2fb1e40b84eb10611
Nicholas Long <nicholas.long@nrel.gov>
2014-06-23 16:15:16 -0600

    add indexes to created at

--- server/app/models/data_point.rb
+++ server/app/models/data_point.rb
@@ -28,6 +28,7 @@ class DataPoint
   index({ id: 1 }, unique: true)
   index(name: 1)
   index(status: 1)
+  index(analysis_id: 1, created_at: 1)
   index(analysis_id: 1)
   index(uuid: 1, status: 1, download_status: 1)
   index(run_start_time: -1, name: 1)
