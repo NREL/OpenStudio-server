6494d742006ea64357f6c5b215e5a5b287063e08
Alex Swindler <Alex.Swindler@nrel.gov>
2014-06-23 10:09:00 -0600

    Bump version

--- server/lib/version.rb
+++ server/lib/version.rb
@@ -1,4 +1,4 @@
 module OpenstudioServer
-  VERSION = '1.7.7'
+  VERSION = '1.7.8'
   VERSION_EXT = '-rc1' # with preceding - or +
 end
--- vagrant/chef/roles/openstudio.rb
+++ vagrant/chef/roles/openstudio.rb
@@ -10,7 +10,7 @@ default_attributes(
     :openstudio => {
         :version => "1.4.0",
         :installer => {
-            :version_revision => "6b1721084f",
+            :version_revision => "60c2e9d96f",
             :platform => "Linux-Ruby2.0"
         }
     }
