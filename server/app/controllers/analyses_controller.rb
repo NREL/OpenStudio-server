853a6f146630990fe81b5cf846affc3e2d494bd4
Nicholas Long <nicholas.long@nrel.gov>
2014-07-23 12:28:54 -0600

    create job model and hook it up

--- server/app/controllers/analyses_controller.rb
+++ server/app/controllers/analyses_controller.rb
@@ -251,7 +251,8 @@ class AnalysesController < ApplicationController
         render json: {
           analysis: {
             status: @analysis.status,
-            analysis_type: @analysis.analysis_type
+            analysis_type: @analysis.analysis_type,
+            jobs: @analysis.jobs.order_by(:index.asc)
           },
           data_points: dps.map { |k| { _id: k.id, status: k.status, final_message: k.status_message } } }
       end
