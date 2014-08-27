f78f6343eb8d2e8bcbe8a3c2580d4d674cc81a32
Nicholas Long <nicholas.long@nrel.gov>
2014-08-27 13:56:31 -0600

    sudo secure_path packages

--- vagrant/chef/cookbooks/openstudio_server/attributes/default.rb
+++ vagrant/chef/cookbooks/openstudio_server/attributes/default.rb
@@ -1,3 +1,5 @@
 default[:openstudio_server][:ruby_path] = "/usr/bin"
 default[:openstudio_server][:server_path] = "/var/www/rails/openstudio"
 default[:openstudio_server][:rails_environment] = "development"
+
+default[:openstudio_server][:ruby_version] = "2.0.0-p481"
--- vagrant/chef/cookbooks/openstudio_server/metadata.rb
+++ vagrant/chef/cookbooks/openstudio_server/metadata.rb
@@ -6,16 +6,22 @@ description       "Installs and configures OpenStudio web application"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "2.0.0"
 
-recipe "passenger_apache2", "Installs Passenger as an Apache module"
-recipe "passenger_apache2::mod_rails", "Enables Apache module configuration for passenger module"
-
-depends "apache2", ">= 1.0.4"
+#recipe "passenger_apache2", "Installs Passenger as an Apache module"
+#recipe "passenger_apache2::mod_rails", "Enables Apache module configuration for passenger module"
+#depends "apache2", ">= 1.0.4"
 
 depends 'apt'
 depends 'cron'
 depends 'ntp'
+depends 'man'
+depends 'ack'
+depends 'vim'
+depends 'zip'
+depends 'sudo'
 depends 'logrotate'
 depends 'build-essential'
+
+# Applications
 depends 'rbenv'
 
 
