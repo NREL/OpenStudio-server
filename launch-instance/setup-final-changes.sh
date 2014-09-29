be1db8a11b9a18c6bc5a82b2d83a38854b585765
Nicholas Long <nicholas.long@nrel.gov>
2014-09-29 11:45:13 -0600

    set initial password and rearrange code

--- launch-instance/setup-final-changes.sh
+++ launch-instance/setup-final-changes.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-# Unlock password (again)
-sudo usermod -U ubuntu
+# Allow password-based authentication
+sudo sed -i 's/PasswordAuthentication.no/PasswordAuthentication\ yes/g' /etc/ssh/sshd_config
+echo StrictHostKeyChecking no > /home/ubuntu/.ssh/config
+sudo service ssh restart
+
+# Set a default password and unlock
+echo -e "ubuntu\nubuntu" | sudo passwd ubuntu
+sudo passwd -u ubuntu
 
 # Clean up files to remove history
 cat /dev/null > ~/.ssh/authorized_keys
@@ -13,12 +19,12 @@ sudo cat /dev/null > /var/www/rails/openstudio/log/delayed_job.log
 sudo rm -f /var/www/rails/openstudio/log/test.log
 sudo rm -rf /var/www/rails/openstudio/public/assets/*
 sudo rm -rf /var/www/rails/openstudio/tmp/*
-sudo cat /dev/null > /var/log/auth.log
-sudo cat /dev/null > /var/log/lastlog
-sudo cat /dev/null > /var/log/kern.log
-sudo cat /dev/null > /var/log/boot.log
-sudo cat /dev/null > /var/log/mongodb/mongodb.log
-sudo cat /dev/null > /var/log/apache2/error.log
+sudo -- su -c 'cat /dev/null > /var/log/auth.log'
+sudo -- su -c 'cat /dev/null > /var/log/lastlog'
+sudo -- su -c 'cat /dev/null > /var/log/kern.log'
+sudo -- su -c 'cat /dev/null > /var/log/boot.log'
+sudo -- su -c 'cat /dev/null > /var/log/mongodb/mongodb.log'
+sudo -- su -c 'cat /dev/null > /var/log/apache2/error.log'
 sudo rm -f /data/launch-instance/*.pem
 sudo rm -f /data/launch-instance/*.log
 sudo rm -f /data/launch-instance/*.json
