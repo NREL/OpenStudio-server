b48b1b9f3434118c3ffd0c8424fc31b104cfdb1e
Nicholas Long <nicholas.long@nrel.gov>
2014-09-29 17:43:24 -0600

    fix awsinfo call

--- launch-instance/setup-server-changes.sh
+++ launch-instance/setup-server-changes.sh
@@ -12,6 +12,7 @@ rake db:mongoid:create_indexes
 rm -rf /mnt/openstudio
 
 # Upgrade any remaining packages and call the cleanup scripts
+sudo apt-get update -y
 sudo apt-get upgrade -y
 chmod +x /data/launch-instance/setup-cleanup-aws.sh
 sudo /data/launch-instance/setup-cleanup-aws.sh
--- launch-instance/setup-worker-changes.sh
+++ launch-instance/setup-worker-changes.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Upgrade any remaining packages and call the cleanup scripts
+sudo apt-get update -y
 sudo apt-get upgrade -y
 chmod +x /data/launch-instance/setup-cleanup-aws.sh
 sudo /data/launch-instance/setup-cleanup-aws.sh
