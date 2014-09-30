a82458b3de9d450f1067786860a6766e30478c44
Nicholas Long <nicholas.long@nrel.gov>
2014-09-29 22:06:15 -0600

    fix ami lookup

--- vagrant/create_vms.rb
+++ vagrant/create_vms.rb
@@ -467,7 +467,6 @@ end
 end
 $threads.each { |t| t.join }
 
-
 puts
 puts "============================= AMI Information======================================="
 puts
@@ -489,8 +488,8 @@ if good_build
     puts "=========================== JSON ========================================="
     amis_hash = {}
     amis_hash[@os_version] = {}
-    amis_hash[@os_version]["server"] = @vms.select { |vm| vm[:name] == "server_aws" }.first[:ami_id]
-    amis_hash[@os_version]["worker"] = @vms.select { |vm| vm[:name] == "worker_aws" }.first[:ami_id]
+    amis_hash[@os_version]["server"] = @vms.select { |vm| vm[:name] == "server" }.first[:ami_id]
+    amis_hash[@os_version]["worker"] = @vms.select { |vm| vm[:name] == "worker" }.first[:ami_id]
     amis_hash[@os_version]["cc2worker"] = @vms.select { |vm| vm[:name] == "worker_cluster" }.first[:ami_id]
 
     puts JSON.pretty_generate(amis_hash)
