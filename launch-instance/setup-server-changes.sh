2fa814d277e9e6555164ab20047d899bbf2dfd86
Nicholas Long <nicholas.long@nrel.gov>
2014-09-29 19:03:32 -0600

    upgrade w/o grub window

--- launch-instance/setup-server-changes.sh
+++ launch-instance/setup-server-changes.sh
@@ -13,6 +13,11 @@ rm -rf /mnt/openstudio
 
 # Upgrade any remaining packages and call the cleanup scripts
 sudo apt-get update -y
+
+# Upgrade without a prompt! The second upgrade is just to make sure everyting was upgraded
+# http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 sudo apt-get upgrade -y
+
 chmod +x /data/launch-instance/setup-cleanup-aws.sh
 sudo /data/launch-instance/setup-cleanup-aws.sh
