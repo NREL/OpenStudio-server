d1cd69b03a608f4295eb91477361392625b30d08
Nicholas Long <nicholas.long@nrel.gov>
2014-10-24 11:31:16 -0600

    use github openstudio install

--- vagrant/chef/roles/openstudio.rb
+++ vagrant/chef/roles/openstudio.rb
@@ -7,11 +7,22 @@ run_list([
 
 
 default_attributes(
+    # Use this for the official release versions
+    # :openstudio => {
+    #     :version => "1.5.1",
+    #     :installer => {
+    #         :version_revision => "297c509238",
+    #         :platform => "Linux-Ruby2.0"
+    #     }
+    # }
+    # Use this for custom installations from any url
     :openstudio => {
+        :origin => 'url',
         :version => "1.5.1",
         :installer => {
-            :version_revision => "297c509238",
-            :platform => "Linux-Ruby2.0"
+            :version_revision => "5d1f0768a2",
+            :download_url => 'https://github.com/NREL/OpenStudio/releases/download/v1.5.1-workflow5',
+            :download_filename => "OpenStudio-1.5.1.5d1f0768a2-Linux.deb"
         }
     }
 )
