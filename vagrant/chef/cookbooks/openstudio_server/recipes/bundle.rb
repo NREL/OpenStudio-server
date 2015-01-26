8b3b831c850388183dea3672b259c75c034e0ee9
Nicholas Long <nicholas.long@nrel.gov>
2015-01-25 22:10:02 -0700

    install worker data on server during provision

--- vagrant/chef/cookbooks/openstudio_server/recipes/bundle.rb
+++ vagrant/chef/cookbooks/openstudio_server/recipes/bundle.rb
@@ -4,7 +4,7 @@
 #
 
 # execute bundle install in the server directory to install all the rails dependencies
-bash 'bundle install' do
+bash 'rails_bundle_install' do
   code <<-EOH
     cd #{node[:openstudio_server][:server_path]}
     bundle install
