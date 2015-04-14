54289ae173712c7d6b32729423b1c007e551c8ed
Nicholas Long <nicholas.long@nrel.gov>
2015-04-14 14:42:29 -0600

    remove unneeded files

--- launch-instance/setup-nohyperthread.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Has this been deprecated? NL - 2014-09-29
-for cpunum in $(
-  cat /sys/devices/system/cpu/cpu*/topology/thread_siblings_list | 
-  cut -s -d, -f2- | tr ',' '\n' | sort -un); do
-    echo 0 > /sys/devices/system/cpu/cpu$cpunum/online
-done
