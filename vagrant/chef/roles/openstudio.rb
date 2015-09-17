791e888a662d90072e6d9e899e18e6212ce7e32e
brianlball <brian.ball@nrel.gov>
2015-09-17 08:39:18 -0600

    bump OS version 1.9.0

--- vagrant/chef/roles/openstudio.rb
+++ vagrant/chef/roles/openstudio.rb
@@ -8,9 +8,9 @@ run_list([
 default_attributes(
   # Use this for the official release versions
   openstudio: {
-    version: '1.8.5',
+    version: '1.9.0',
     installer: {
-      version_revision: 'e1a622b890'
+      version_revision: '043ddbb575'
     }
   }
 
