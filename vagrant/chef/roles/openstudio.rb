427d18d3cf245b3dcdb16eae857e3d2e0502d6fa
Rob Guglielmetti <rob.guglielmetti@nrel.gov>
2016-03-30 14:09:26 -0600

    revert OS version

--- vagrant/chef/roles/openstudio.rb
+++ vagrant/chef/roles/openstudio.rb
@@ -8,9 +8,9 @@ run_list([
 default_attributes(
   # Use this for the official release versions
   openstudio: {
-    version: '1.10.0',
+    version: '1.10.6',
     installer: {
-      version_revision: 'bc05249524'
+      version_revision: '689a394736'
     }
   }
 
