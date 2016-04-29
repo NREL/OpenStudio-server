d6832f9468f563eb478fae706a74cee9f1f16dbb
Nicholas Long <nicholas.long@nrel.gov>
2016-04-29 13:51:54 -0600

    remove old worker node spec

--- server/spec/models/run_simulation_data_point_spec.rb
+++ server/spec/models/run_simulation_data_point_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe RunSimulateDataPoint, type: :model do
 
     # expect(Delayed::Job.count).to eq(1)
   end
+
+  it 'should sort worker jobs correctly' do
+    a = %w(00_Job0 01_Job1 11_Job11 20_Job20 02_Job2 21_Job21)
+
+    a.sort!
+
+    expect(a.first).to eq '00_Job0'
+    expect(a.last).to eq '21_Job21'
+    expect(a[3]).to eq '11_Job11'
+  end
 end
