2e194a00662fb495d80c232839c7aec0fb490e4e
Ry Horsey <horsey.henry@gmail.com>
2016-05-18 11:43:28 -0600

    Merging master

--- vagrant/chef/roles/openstudio.rb
+++ vagrant/chef/roles/openstudio.rb
@@ -8,9 +8,9 @@ run_list([
 default_attributes(
   # Use this for the official release versions
   openstudio: {
-    version: '1.10.6',
+    version: '1.11.3',
     installer: {
-      version_revision: '689a394736'
+      version_revision: '3bdae23c47'
     }
   }
 
