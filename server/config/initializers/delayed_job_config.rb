7ef2011e212279a0d344c349c6a0784ae0f83a29
Nicholas Long <nicholas.long@nrel.gov>
2016-05-24 15:27:50 -0600

    remove remaining capistrano

--- server/config/initializers/delayed_job_config.rb
+++ server/config/initializers/delayed_job_config.rb
@@ -1,2 +1,3 @@
-# Allow the jobs to run for up to 1 week.  If this is ever hit, then we have other problems.
+# Allow the jobs to run for up to 1 week.  If this is ever hit, then we have
+# other problems.
 Delayed::Worker.max_run_time = 168.hours
