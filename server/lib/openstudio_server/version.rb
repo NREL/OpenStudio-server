f3798fb492ba50d46bdfdb2510fe6277a724d544
Evan Weaver <evan.weaver@nrel.gov>
2016-06-28 13:43:44 -0600

    Bump OpenStudio version to OS.1.12.0.ef50b89958

--- server/lib/openstudio_server/version.rb
+++ server/lib/openstudio_server/version.rb
@@ -34,6 +34,6 @@
 #*******************************************************************************
 
 module OpenstudioServer
-  VERSION = '1.18.0'
-  VERSION_EXT = '-rc0' # with preceding - or +
+  VERSION = '1.19.0'
+  VERSION_EXT = '-OS.1.12.0.ef50b89958' # with preceding - or +
 end
