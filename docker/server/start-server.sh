08c061dcc7ce37425a3d415d4c4fcd04e49f6827
Nicholas Long <nicholas.long@nrel.gov>
2016-07-15 11:30:48 -0600

    move to opt/openstudio for server, cli, and r code

--- docker/server/start-server.sh
+++ docker/server/start-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Always create new indexes in case the models have changed
-cd /srv && bundle exec rake db:mongoid:create_indexes
+cd /opt/openstudio/server && bundle exec rake db:mongoid:create_indexes
 
 /opt/nginx/sbin/nginx
