4e4deb74dfc70b7bf3d45e69f289ae12d5fcce34
Ry Horsey <horsey.henry@gmail.com>
2016-11-02 17:09:17 -0600

    Starting with install_gems on OSX

--- travis/script.sh
+++ travis/script.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -ev
 if [ "${REDHAT_BUILD}" = "false" ]; then
-	echo 'IN A MAC BUILD'
+	if [ "${OSX_BUILD}" = "true" ]; then
+		brew install mongo
+		ruby ./bin/openstudio_meta install_gems --with_test_develop --debug --verbose
+	fi
+	if [ "${OSX_BUILD}" = "false"]; then
+		sudo apt-get update
+		sudo apt-get upgrade -y
+	fi
 fi
 if [ "${REDHAT_BUILD}" = "true" ]; then
 	echo 'IN A REDHAT BUILD'
