9439bf749f0c6f23fe6353f1a6340448f49f5d77
Ry Horsey <horsey.henry@gmail.com>
2016-11-02 20:21:55 -0600

    Unset BUNDLE_GEMFILE and more, adding in first pass at a centos7 container for HPC testing.

--- travis/script.sh
+++ travis/script.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -ev
 if [ "${REDHAT_BUILD}" = "false" ]; then
-	if [ "${OSX_BUILD}" = "true" ]; then
+	if [ "TRAVIS_OS_NAME" = "osx" ]; then
 		brew install mongo
-		launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist
+		mongod --config /usr/local/etc/mongod.conf
+		unset BUNDLE_GEMFILE
 		ruby ./bin/openstudio_meta install_gems --with_test_develop --debug --verbose
 	fi
-	if [ "${OSX_BUILD}" = "false"]; then
+	if [ "TRAVIS_OS_NAME" != "osx"]; then
 		sudo apt-get update
 		sudo apt-get upgrade -y
 	fi
 fi
 if [ "${REDHAT_BUILD}" = "true" ]; then
 	echo 'IN A REDHAT BUILD'
+	docker pull ${OS_FLAVOR}:${OS_VERSION}
+	CONTAINER_ID=$(mktemp)
+	docker run --detach --volume="${PWD}":/home/openstudio-server ${RUN_OPTS} ${OS_FLAVOR}:${OS_VERSION} "/usr/lib/systemd/systemd" > "${CONTAINER_ID}"
+	docker exec --tty "$(cat ${container_id})" env TERM=xterm ruby ~/openstudio-server/bin/openstudio_meta install_gems --with_test_develop --debug --verbose
 fi
