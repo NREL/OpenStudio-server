f93a3c8070aa7a3a5ce03d8edb886061b9b9373e
Ry Horsey <horsey.henry@gmail.com>
2016-11-25 09:09:12 -0700

    Adding mongod to the path, and including version info.

--- ci/appveyor/setup.cmd
+++ ci/appveyor/setup.cmd
@@ -2,7 +2,7 @@
   mkdir c:\projects\openstudio
   7z x OpenStudio2-1.13.0.fb588cc683-win32.zip -oc:\projects\openstudio
   set RUBYLIB=C:\projects\openstudio\Ruby
-  set PATH=C:\Ruby%RUBY_VERSION%\bin;%PATH%
+  set PATH=C:\Ruby%RUBY_VERSION%\bin;C:\Mongodb\bin;%PATH%
   C:\Ruby%RUBY_VERSION%\bin\ruby C:\Ruby%RUBY_VERSION%\bin\gem update --system
   C:\Ruby%RUBY_VERSION%\bin\ruby c:\projects\openstudio-server\bin\openstudio_meta install_gems --with_test_develop --debug --verbose
   C:\Ruby%RUBY_VERSION%\bin\bundle install --gemfile=C:\projects\openstudio-server\Gemfile
