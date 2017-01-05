899331d0802834f9642d100f20b6e6755398a67a
Brian Ball <Brian.Ball@nrel.gov>
2017-01-05 10:49:18 -0700

    remove nrcan standards

--- server/lib/openstudio_server/version.rb
+++ server/lib/openstudio_server/version.rb
@@ -34,6 +34,6 @@
 #*******************************************************************************
 
 module OpenstudioServer
-  VERSION = '1.21.1-nrcan'
+  VERSION = '1.21.1'
   VERSION_EXT = '' # with preceding - or +
 end
