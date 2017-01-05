135704159775132b40f16fea3b1dc8a7de0dbf2b
Brian Ball <Brian.Ball@nrel.gov>
2017-01-05 11:45:19 -0700

    noko 1.6.8

--- server/lib/openstudio_server/version.rb
+++ server/lib/openstudio_server/version.rb
@@ -34,6 +34,6 @@
 #*******************************************************************************
 
 module OpenstudioServer
-  VERSION = '1.21.3-nrcan'
+  VERSION = '1.21.4-nrcan'
   VERSION_EXT = '' # with preceding - or +
 end
