7b92af84f46cd7a883403342508d8a46b6eea140
Brian Ball <Brian.Ball@nrel.gov>
2017-01-05 10:24:57 -0700

    nrcan version of standards

--- server/lib/openstudio_server/version.rb
+++ server/lib/openstudio_server/version.rb
@@ -34,6 +34,6 @@
 #*******************************************************************************
 
 module OpenstudioServer
-  VERSION = '1.21.0'
+  VERSION = '1.21.0-nrcan'
   VERSION_EXT = '' # with preceding - or +
 end
