10b262a082b9ac37045888dd75f025cbe1fb2e9c
Ry Horsey <horsey.henry@gmail.com>
2017-01-31 15:38:50 -0700

    One more fix for the lvm.

--- docker/deployment/scripts/server_provision.sh
+++ docker/deployment/scripts/server_provision.sh
@@ -6,8 +6,10 @@ echo "Expanding the logical volume docker-thinpool"
 echo "------------------------------------------------------------------------"
 echo ""
 sleep 1
-old_sectors="$(sudo blockdev --getsize64 /dev/docker/thinpool)"
-echo "Original sector count for 'docker-thinpool' is $old_sectors"
+old_sectors="$(($(sudo blockdev --getsize64 /dev/docker/thinpool)/512))"
+echo "Original 512 sector count for 'docker-thinpool' is $old_sectors"
+docker_thinpool_table="$(sudo dmsetup table docker-thinpool)"
+echo "Original devicemapper table for 'docker-thinpool' is: \"$docker_thinpool_table\""
 if [ "$(sudo lsblk -o NAME | grep xvda1)" = '└─xvda1' ]; then
 	sudo vgextend docker -y /dev/xvdb
 	sudo vgextend docker -y /dev/xvdc
@@ -25,9 +27,7 @@ else
 fi
 sudo lvextend -l+100%FREE -n docker/thinpool
 new_sectors="$(($(sudo blockdev --getsize64 /dev/docker/thinpool)/512))"
-echo "New sector count for 'docker-thinpool' is $new_sectors"
-docker_thinpool_table="$(sudo dmsetup table docker-thinpool)"
-echo "Original devicemapper table for 'docker-thinpool' is: \"$docker_thinpool_table\""
-new_table=${docker_thinpool_table/"3166724096"/${new_sectors}}
+echo "New 512 sector count for 'docker-thinpool' is $new_sectors"
+new_table=${docker_thinpool_table/${old_sectors}/${new_sectors}}
 echo "New devicemapper table for 'docker-thinpool' will be: \"$new_table\""
 sudo dmsetup suspend docker-thinpool && sudo dmsetup reload docker-thinpool --table "$new_table" && sudo dmsetup resume docker-thinpool
