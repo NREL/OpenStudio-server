ac551896af303369502e196278ab3787a1093f88
Ry Horsey <horsey.henry@gmail.com>
2017-01-30 19:46:49 -0700

    Hardening lvm commands to wipe ext3 when required.

--- docker/deployment/scripts/server_provision.sh
+++ docker/deployment/scripts/server_provision.sh
@@ -9,19 +9,19 @@ sleep 1
 old_sectors="$(sudo blockdev --getsize64 /dev/docker/thinpool)"
 echo "Original sector count for 'docker-thinpool' is $old_sectors"
 if [ "$(sudo lsblk -o NAME | grep xvda1)" = '└─xvda1' ]; then
-	sudo vgextend docker /dev/xvdb
-	sudo vgextend docker /dev/xvdc
-	sudo vgextend docker /dev/xvdd
-	sudo vgextend docker /dev/xvde
-	sudo vgextend docker /dev/xvdf
-	sudo vgextend docker /dev/xvdg
+	sudo vgextend docker -y /dev/xvdb
+	sudo vgextend docker -y /dev/xvdc
+	sudo vgextend docker -y /dev/xvdd
+	sudo vgextend docker -y /dev/xvde
+	sudo vgextend docker -y /dev/xvdf
+	sudo vgextend docker -y /dev/xvdg
 else
-	sudo vgextend docker /dev/sdb
-	sudo vgextend docker /dev/sdc
-	sudo vgextend docker /dev/sdd
-	sudo vgextend docker /dev/sde
-	sudo vgextend docker /dev/sdf
-	sudo vgextend docker /dev/sdg
+	sudo vgextend docker -y /dev/sdb
+	sudo vgextend docker -y /dev/sdc
+	sudo vgextend docker -y /dev/sdd
+	sudo vgextend docker -y /dev/sde
+	sudo vgextend docker -y /dev/sdf
+	sudo vgextend docker -y /dev/sdg
 fi
 sudo lvextend -l+100%FREE -n docker/thinpool
 new_sectors="$(($(sudo blockdev --getsize64 /dev/docker/thinpool)/512))"
