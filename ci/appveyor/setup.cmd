483457d1aaac32c10b2eb0dfd6ea19d47ec8030c
Ry Horsey <horsey.henry@gmail.com>
2017-02-21 12:10:11 -0700

    Sadly having to install and config 2.2.4

--- ci/appveyor/setup.cmd
+++ ci/appveyor/setup.cmd
@@ -1,6 +1,11 @@
   curl -SLO https://openstudio-resources.s3.amazonaws.com/pat-dependencies/OpenStudio2-1.13.0.fb588cc683-win32.zip
   mkdir c:\projects\openstudio
   7z x OpenStudio2-1.13.0.fb588cc683-win32.zip -oc:\projects\openstudio
+  curl -SLO https://dl.bintray.com/oneclick/rubyinstaller/ruby-2.2.4-x64-mingw32.7z
+  mkdir c:\Ruby224-x64
+  7z x ruby-2.2.4-x64-mingw32.7z -oc:\Ruby224-x64
+  copy /y c:\projects\openstudio-server\ci\appveyor\config.yml c:\Ruby21\DevKit\config.yml
+  ruby c:\Ruby21\DevKit\dk.rb install
   set RUBYLIB=C:\projects\openstudio\Ruby
   set PATH=C:\Ruby%RUBY_VERSION%\bin;C:\Mongodb\bin;%PATH%
   C:\Ruby%RUBY_VERSION%\bin\ruby C:\Ruby%RUBY_VERSION%\bin\gem update --system
