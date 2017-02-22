9a922d4a6b1949eeea7a0d3abbcf1148f5182658
Brian Ball <Brian.Ball@nrel.gov>
2017-02-21 19:29:15 -0700

    replace = with <-

--- docker/R/lib/functions.R
+++ docker/R/lib/functions.R
@@ -1,9 +1,12 @@
-check_run_flag = function(script_path, host_url, analysis_id){
-    y = paste('ruby ',script_path,'/api_get_status.rb -h ',host_url,' -a ',analysis_id,sep='')
+check_run_flag <- function(script_path, host_url, analysis_id){
+    y <- paste('ruby ',script_path,'/api_get_status.rb -h ',host_url,' -a ',analysis_id,sep='')
     print(paste('run command', y))
-    z = system(y,intern=TRUE)
-    z = z[length(z)] # Get last line of output
+    z <- system(y,intern=TRUE)
+    z <- z[length(z)] # Get last line of output
 
     json = try(fromJSON(z), silent=TRUE)
+    print(paste("run_flag_json:",json))
+    print(paste('is.recursive(run_flag_json):',is.recursive(json)))
+    print(paste('is.atomic(run_flag_json):',is.atomic(json)))
     return(json$result$analysis$run_flag)
 }
