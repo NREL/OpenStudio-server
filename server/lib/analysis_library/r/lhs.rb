0103debcd4f9c97e5d41adaa93f3f7772e0a09aa
rHorsey <horsey.henry@gmail.com>
2017-07-13 22:44:51 -0600

    Revert "update nginx to serve images correctly. use cairo"
    This reverts commit 8db8fbdf194a0436d08f6728d80daacd794e323b.

--- server/lib/analysis_library/r/lhs.rb
+++ server/lib/analysis_library/r/lhs.rb
@@ -244,12 +244,10 @@ module AnalysisLibrary::R
       if samples && samples.count > 0
         save_file_name = "#{APP_CONFIG['server_asset_path']}/R/#{Dir::Tmpname.make_tmpname(['r_samples_plot', '.png'], nil)}"
         logger.info("R image filename is #{save_file_name}")
-        # If running on Docker, then use type='cairo' to create PNG (since it is headless and cairo is installed)
-        png_type = Rails.env =~ /docker/ ? ', type="cairo"' : ''
         if samples[0].is_a?(Float) || samples[0].is_a?(Integer)
           @r.command(d: { samples: samples }.to_dataframe) do
             %{
-              png(filename="#{save_file_name}", width = 1024, height = 1024#{png_type})
+              png(filename="#{save_file_name}", width = 1024, height = 1024)
               hist(d$samples, freq=F, breaks=20)
               dev.off()
             }
@@ -257,7 +255,7 @@ module AnalysisLibrary::R
         else # plot as a table
           @r.command(d: { samples: samples }.to_dataframe) do
             %{
-              png(filename="#{save_file_name}", width = 1024, height = 1024#{png_type})
+              png(filename="#{save_file_name}", width = 1024, height = 1024)
               plot(table(d$samples), ylab='count')
               dev.off()
             }
