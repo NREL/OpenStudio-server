709952b399b016b536231d52c3f66d315ef99091
rHorsey <horsey.henry@gmail.com>
2017-12-18 09:49:44 -0700

    Staging for 2.3.2 AMI release.

--- server/lib/openstudio_server/version.rb
+++ server/lib/openstudio_server/version.rb
@@ -34,6 +34,6 @@
 # *******************************************************************************
 
 module OpenstudioServer
-  VERSION = '2.3.1'.freeze
+  VERSION = '2.3.2'.freeze
   VERSION_EXT = ''.freeze # with preceding - or +
 end
