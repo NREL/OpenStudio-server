d46def2967e31c01faa2cf480e7f39b06efc6c45
rHorsey <horsey.henry@gmail.com>
2017-12-19 01:08:43 -0700

    Misspelled baseline to test for adding OOM protection layer to the web container.

--- docker/server/start-server.sh
+++ docker/server/start-server.sh
@@ -3,4 +3,6 @@
 # Always create new indexes in case the models have changed
 cd /opt/openstudio/server && bundle exec rake db:mongoid:create_indexes
 
+ruby /usr/local/bin/memfix-controler.rb start
+
 /opt/nginx/sbin/nginx
