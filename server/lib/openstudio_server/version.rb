821a5268d73c6935af487e0ef29d08b54afc962a
rHorsey <horsey.henry@gmail.com>
2017-12-21 00:08:44 -0700

    Fix a number of packaging related issues and update to Docker CE 17.09.1

--- server/lib/openstudio_server/version.rb
+++ server/lib/openstudio_server/version.rb
@@ -34,6 +34,6 @@
 # *******************************************************************************
 
 module OpenstudioServer
-  VERSION = '2.3.1'.freeze
-  VERSION_EXT = ''.freeze # with preceding - or +
+  VERSION = '2.2.1'.freeze
+  VERSION_EXT = '-ls2'.freeze # with preceding - or +
 end
