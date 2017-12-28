b0b16f959630eea7fc5e716f184ef35c256aeb55
rHorsey <horsey.henry@gmail.com>
2017-12-28 13:54:40 -0700

    Restrict initializers for local and docker builds.

--- server/config/initializers/delayed_job_config.rb
+++ server/config/initializers/delayed_job_config.rb
@@ -33,16 +33,18 @@
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # *******************************************************************************
 
-# Allow the jobs to run for up to 1 week.  If this is ever hit, then we have other problems.
-# Delayed::Worker.destroy_failed_jobs = false
-# Delayed::Worker.sleep_delay = 60
-# Delayed::Worker.max_attempts = 3
+if ['development', 'local', 'local-test'].include? Rails.env
+  # Allow the jobs to run for up to 1 week.  If this is ever hit, then we have other problems.
+  # Delayed::Worker.destroy_failed_jobs = false
+  # Delayed::Worker.sleep_delay = 60
+  # Delayed::Worker.max_attempts = 3
 
-Delayed::Worker.max_run_time = 168.hours
-# Delayed::Worker.read_ahead = 10
-# Delayed::Worker.default_queue_name = 'default'
-# Delayed::Worker.delay_jobs = !Rails.env.test?
-Delayed::Worker.raise_signal_exceptions = :term
-Delayed::Worker.logger = Logger.new(File.join(APP_CONFIG['rails_log_path'], 'delayed_job.log'))
+  Delayed::Worker.max_run_time = 168.hours
+  # Delayed::Worker.read_ahead = 10
+  # Delayed::Worker.default_queue_name = 'default'
+  # Delayed::Worker.delay_jobs = !Rails.env.test?
+  Delayed::Worker.raise_signal_exceptions = :term
+  Delayed::Worker.logger = Logger.new(File.join(APP_CONFIG['rails_log_path'], 'delayed_job.log'))
 
-# Delayed::Worker.plugins << Delayed::TaggedLogging::Plugin
+  # Delayed::Worker.plugins << Delayed::TaggedLogging::Plugin
+end
