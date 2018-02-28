8dc7f1424a4bd2b3d48b63dbd1864626ed511d11
rHorsey <horsey.henry@gmail.com>
2018-02-28 14:27:38 -0700

    Forgot the r in bundler

--- ci/appveyor/unit-test.cmd
+++ ci/appveyor/unit-test.cmd
@@ -3,6 +3,6 @@ set PATH=C:\Ruby%RUBY_VERSION%\bin;C:\Mongodb\bin;%PATH%
 cd c:\projects\openstudio-server
 echo Running unit tests against local server
 mkdir C:\projects\openstudio-server\spec\unit-test\
-gem install bundle -v 1.14.4
+gem install bundler -v 1.14.4
 bundle install --with default development test
 ruby bundle exec rspec -e 'unit tests'
