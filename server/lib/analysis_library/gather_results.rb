63fd29cf30f06ae6573324007838705759d1f478
padmassun <padmassun@gmail.com>
2018-05-15 09:26:46 -0400

    change git-ting of the results repo

--- server/lib/analysis_library/gather_results.rb
+++ server/lib/analysis_library/gather_results.rb
@@ -7,13 +7,15 @@ def git_clean_rep(url,options)
   folder =File.join('/mnt/openstudio/server/assets', basename)
   FileUtils.rm_rf(folder)
   puts "Creating clone of #{url} with options  #{options}."
-  Git.clone(url,folder,options)
+  %x[git clone #{url} #{folder}]
+  #Git.clone(url,folder,options)
   return folder
 end
 
 def zip_all_results(uuid, cores=1)
-  
+
   repo_folder = git_clean_rep('https://github.com/canmet-energy/btap_gather_results.git', options = { })
+  puts repo_folder
   # %x[git clone https://github.com/canmet-energy/btap_gather_results.git /mnt/openstudio/server/assets/btap_gather_results]
   require "#{repo_folder}/gather_results.rb"
   start_gather_result(uuid, cores)
@@ -22,4 +24,3 @@ def zip_all_results(uuid, cores=1)
   # Finish up
   puts 'SUCCESS'
 end
-
