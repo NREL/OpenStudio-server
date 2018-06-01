ad032f4c7c63a972be8d838d8cc58b5b75f67c80
Padmassun Rajakareyar <padmassun@gmail.com>
2018-06-01 10:11:20 -0400

    Added file size to HTML response header - BTAP Results

--- server/app/controllers/analyses_controller.rb
+++ server/app/controllers/analyses_controller.rb
@@ -727,6 +727,8 @@ class AnalysesController < ApplicationController
     path = "/mnt/openstudio/server/assets/results.#{@analysis.id}.zip";
 
     if File.exist?(path)
+      file_size = File.size(path)
+      response.headers['Content-Length'] = file_size.to_s
       send_data File.open(path, 'rb').read, filename: "results.#{@analysis.id}.zip", type: "application/zip", disposition: 'attachment'
     end
   end
