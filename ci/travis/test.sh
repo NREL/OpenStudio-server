69ebdbd67d9093219f13067b4d514affcdeae7f5
Petersen, Anya <Anya.Petersen@nrel.gov>
2018-06-02 15:33:22 -0600

    platform-specific config + remove unused spec

--- ci/travis/test.sh
+++ ci/travis/test.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
-# do not check platform - commands should be identical for Ubuntu vs OSX
+
+# platform-specific config here:
+if [ "${BUILD_ARCH}" == "OSX" ]; then
+    export RUBYLIB="${HOME}/openstudio/Ruby/"
+    home_dir="/Users/travis/build/NREL/OpenStudio-server"
+elif [ "${BUILD_ARCH}" == "OSX" ]; then
+    export RUBYLIB="/usr/Ruby"
+    home_dir="/home/travis/build/NREL/OpenStudio-server"
+fi
 
 # run unit tests via openstudio_meta run_rspec command which attempts to reproduce the PAT local environment
 # prior to running tests, so we should not set enviroment variables here
 if [ "${BUILD_TYPE}" == "test" ];then
 #    run_rspec command uses this directory for output files, which will be printed in event of failure
-    mkdir ./spec/unit-test
+    mkdir "$home_dir/spec/unit-test"
     echo "starting unit tests"
-    ruby "/Users/travis/build/NREL/OpenStudio-server/bin/openstudio_meta" run_rspec --debug --verbose --mongo-dir="/usr/local/bin" "/Users/travis/build/NREL/OpenStudio-server/spec/unit-test"
+    ruby "$home_dir/bin/openstudio_meta" run_rspec --debug --verbose --mongo-dir="/usr/local/bin" "$home_dir/spec/unit-test"
     exit_status=$?
     if [ $exit_status == 0 ];then
         echo "Completed unit tests successfully"
@@ -18,8 +26,8 @@ if [ "${BUILD_TYPE}" == "test" ];then
     exit $exit_status
 elif [ "${BUILD_TYPE}" == "integration" ]; then
     #    run the analysis integration specs - everything in root directory
-    export RUBYLIB="${HOME}/openstudio/Ruby/"
     #    use same environment as PAT
+    # AP do we need this or is this handled by the openstudio_meta build + start_server and stop_server commands?
     export RAILS_ENV=local
     export SKIP_COVERALLS=true
     #    explicitly set directory.  Probably unnecessary
@@ -27,7 +35,7 @@ elif [ "${BUILD_TYPE}" == "integration" ]; then
     echo 'Beginning integration tests'
     bundle exec rspec; (( exit_status = exit_status || $? ))
     if [ $exit_status -ne 0 ]; then
-        for F in /Users/travis/build/NREL/OpenStudio-server/spec/files/logs/*; do
+        for F in "$home_dir/spec/files/logs/*"; do
             echo "Deleting $F to limit verbosity in case of unit test failure"
             rm $F
         done
