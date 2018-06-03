6b9619359bcda7d5d5fa875e0b69e5d383cf7e1e
Petersen, Anya <Anya.Petersen@nrel.gov>
2018-06-02 23:42:28 -0600

    absolute path to openstudio_meta to run install_gems

--- ci/travis/setup.sh
+++ ci/travis/setup.sh
@@ -31,12 +31,11 @@ fi
 # ? must run after RUBYLIB is set?
 tree ${HOME}/openstudio/Ruby
 
-export RUBYLIB="${HOME}/openstudio/Ruby:$RUBYLIB"
-
-ruby ./bin/openstudio_meta install_gems --with_test_develop --debug --verbose --use_cached_gems
-#used in test.sh
+export RUBYLIB="${RUBYLIB}:${HOME}/openstudio/Ruby"
 export BUILD_DIR="${HOME}/build/NREL/OpenStudio-server"
+ruby .${BUILD_DIR}/bin/openstudio_meta install_gems --with_test_develop --debug --verbose --use_cached_gems
+
 # create dir for output files which will be generated in case of failure
-mkdir "$BUILD_DIR/spec/unit-test"
+mkdir "${BUILD_DIR}/spec/unit-test"
 # Do not report coverage from these build, use the build from CircleCI with no excluded tags
 export SKIP_COVERALLS=true
