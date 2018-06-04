d0706c72646b444d3053678e6383e2622160be9a
Petersen, Anya <Anya.Petersen@nrel.gov>
2018-06-04 15:49:11 -0600

    Small cleanup

--- ci/travis/setup.sh
+++ ci/travis/setup.sh
@@ -16,23 +16,17 @@ if [ "${BUILD_ARCH}" == "OSX" ]; then
     rm -rf $HOME/openstudio
     # Will install into $HOME/openstudio and RUBYLIB will be $HOME/openstudio/Ruby
     sudo ./OpenStudio-$OPENSTUDIO_VERSION.$OPENSTUDIO_VERSION_SHA-Darwin.app/Contents/MacOS/OpenStudio-$OPENSTUDIO_VERSION.$OPENSTUDIO_VERSION_SHA-Darwin --script ci/travis/install-mac.qs
-
+    tree ${HOME}/openstudio/Ruby
 #    mongo_dir="/usr/local/bin"
 elif [ "${BUILD_ARCH}" == "Ubuntu" ]; then
     echo "Setting up Ubuntu for unit tests and Rubocop"
     # install pipe viewer to throttle printing logs to screen (not a big deal in linux, but it is in osx)
-    sudo apt-get install -y pv tree
+    sudo apt-get install -y pv
     mkdir -p reports/rspec
     ./docker/deployment/scripts/install_openstudio.sh $OPENSTUDIO_VERSION $OPENSTUDIO_VERSION_SHA
 
 fi
 
-# We are testing for PAT so all tests will be run by openstudio_meta and require install_gems
-# ? must run after RUBYLIB is set?
-tree ${HOME}/openstudio/Ruby
-
-#export RUBYLIB="${RUBYLIB}:${HOME}/openstudio/Ruby"
-#export BUILD_DIR="${HOME}/build/NREL/OpenStudio-server"
 ruby "${TRAVIS_BUILD_DIR}/bin/openstudio_meta" install_gems --with_test_develop --debug --verbose --use_cached_gems
 
 # create dir for output files which will be generated in case of failure
