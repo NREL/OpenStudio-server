c2bc8b53efd4137057367984aec9a476bff13f30
Petersen, Anya <Anya.Petersen@nrel.gov>
2018-06-14 10:44:51 -0600

    docker-related updates for analysis_wrappers queue

--- docker/server/start-server.sh
+++ docker/server/start-server.sh
@@ -5,4 +5,7 @@ cd /opt/openstudio/server && bundle exec rake db:mongoid:create_indexes
 
 ruby /usr/local/lib/memfix-controller.rb start
 
+# AP hack for now to ensure both resque and nginx/rails are running on web
+bundle exec environment resque:work &
+
 /opt/nginx/sbin/nginx
