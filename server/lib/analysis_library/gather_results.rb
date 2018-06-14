0cdae5a046bd54bcd204cfe64513c2477d2380ac
padmassun <padmassun@gmail.com>
2018-06-14 12:51:53 -0400

    Added bundle install and bundle exec ruby

--- server/lib/analysis_library/gather_results.rb
+++ server/lib/analysis_library/gather_results.rb
@@ -7,7 +7,7 @@ def git_clean_rep(url,options)
   folder =File.join('/mnt/openstudio/server/assets', basename)
   FileUtils.rm_rf(folder)
   puts "Creating clone of #{url} with options  #{options}."
-  system("git clone #{url} #{folder}")
+  system("git clone #{url} #{folder} -b nrcan_60")
   #Git.clone(url,folder,options)
   return folder
 end
@@ -19,8 +19,10 @@ def zip_all_results(uuid, cores=1)
   # %x[git clone https://github.com/canmet-energy/btap_gather_results.git /mnt/openstudio/server/assets/btap_gather_results]
   #require "#{repo_folder}/gather_results"
   #start_gather_result(uuid, cores)
-  system("ruby /mnt/openstudio/server/assets/btap_gather_results/gather_results.rb -a #{uuid}" )
-
+  system("pwd && cd /mnt/openstudio/server/assets/btap_gather_results/ && bundle install ")
+  system('pwd')
+  system("cd /mnt/openstudio/server/assets/btap_gather_results/ && bundle exec ruby ./gather_results.rb -a #{uuid}" )
+  system("pwd")
   # Finish up
   puts 'SUCCESS'
 end
