95f36b4f779a90873971cfa730660b813b3623a9
Petersen, Anya <Anya.Petersen@nrel.gov>
2018-07-12 11:20:05 -0600

    Corrected command

--- docker/server/start-server.sh
+++ docker/server/start-server.sh
@@ -6,6 +6,6 @@ cd /opt/openstudio/server && bundle exec rake db:mongoid:create_indexes
 ruby /usr/local/lib/memfix-controller.rb start
 
 # AP hack for now to ensure both resque and nginx/rails are running on web
-bundle exec environment resque:work &
+bundle exec rake environment resque:work &
 
 /opt/nginx/sbin/nginx
