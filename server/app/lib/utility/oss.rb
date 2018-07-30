43eebb4267b37eae4f429f53e2a3d8d16b7701c0
Nicholas Long <nicholas.long@nrel.gov>
2018-07-30 11:12:35 -0600

    unit tests for oss run script

--- server/app/lib/utility/oss.rb
+++ server/app/lib/utility/oss.rb
@@ -1,5 +1,26 @@
 module Utility
   class Oss
+    # Set some env_vars from the running env var list, ignore the rest
+    #
+    # Why are these all class methods?
+    def self.resolve_env_vars(env_vars)
+      # List of items to keep as regex
+      keep_starts_with = [/^RUBY/, /^BUNDLE/, /^GEM/, /^RAILS_ENV/, /PATH/]
+
+      new_env_vars = {}
+      ENV.each do |var, value|
+        if keep_starts_with.find { |regex| var =~ regex }
+          new_env_vars[var] = value
+        end
+      end
+
+      # overwrite any custom env vars
+      env_vars.each do |var, value|
+        new_env_vars[var] = value
+      end
+
+      new_env_vars
+    end
 
     # Load args from file.  Returns array or nil.
     def self.load_args full_path
@@ -9,6 +30,8 @@ module Utility
     end
 
     # Run script identified by full_path.
+    #
+    # Note that if the spawned_log_path is nil, then this will not execute!
     def self.run_script full_path, timeout = nil, env_vars = {}, args_array = nil, logger = Rails.logger, spawned_log_path = nil
       begin
         logger.debug "updating permissions for #{full_path}"
@@ -21,10 +44,11 @@ module Utility
         logger.debug "running #{full_path}"
         # Spawn the process and wait for completion. Note only the specified env vars are available in the subprocess
         # TODO: handle nil timeout - don't interrupt
-        # NOTE! -- I'm leaving the env vars set so that the user can access rails / models. If we unsetenv vars, then
-        # we need to figure out the bare list of env vars that we need to copy to the new ones in order to keep
-        # rails, bundler, gem, etc working.
-        pid = spawn(env_vars, full_path, *args_array, [:out, :err] => spawned_log_path, :unsetenv_others => false)
+
+        # grab some env vars out of the system env vars in order to run rails runner.
+        env_vars = Utility::Oss.resolve_env_vars(env_vars)
+
+        pid = spawn(env_vars, full_path, *args_array, [:out, :err] => spawned_log_path, :unsetenv_others => true)
         Timeout.timeout(timeout) do
           Process.wait pid
         end
---
+++ server/spec/models/oss_spec.rb
@@ -0,0 +1,100 @@
+# *******************************************************************************
+# OpenStudio(R), Copyright (c) 2008-2018, Alliance for Sustainable Energy, LLC.
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# (1) Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# (2) Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# (3) Neither the name of the copyright holder nor the names of any contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission from the respective party.
+#
+# (4) Other than as required in clauses (1) and (2), distributions in any form
+# of modifications or other derivative works may not use the "OpenStudio"
+# trademark, "OS", "os", or any other confusingly similar designation without
+# specific prior written permission from Alliance for Sustainable Energy, LLC.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER, THE UNITED STATES
+# GOVERNMENT, OR ANY CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# *******************************************************************************
+
+require 'rails_helper'
+
+RSpec.describe Utility::Oss, type: :model do
+  context 'arguments' do
+    before :each do
+      @example_arg = { arg_1: 525600, arg_2: 'string', arg_3: 3.14 }
+    end
+
+    it 'should not load incorrect arguments' do
+      tempfile = Tempfile.new('wrong_arguments.txt')
+      tempfile.write(JSON.pretty_generate(@example_arg))
+      tempfile.close
+      args = Utility::Oss.load_args tempfile.path
+      expect(args).to eq nil
+      tempfile.unlink
+    end
+
+    it 'should load correct arguments' do
+      tempfile = Tempfile.new('right_arguments.txt')
+      tempfile.write(@example_arg.values)
+      tempfile.close
+
+      # with tempfile
+      args = Utility::Oss.load_args tempfile.path
+      expect(args).to eq [525600, 'string', 3.14]
+      tempfile.unlink
+    end
+  end
+
+  context 'run_script' do
+    before :each do
+      @log_file = File.expand_path('oss_spec.log', File.dirname(__FILE__))
+      @example_script = 'echo "successfully ran"'
+    end
+
+    after :each do
+      File.delete(@log_file) if File.exist? @log_file
+    end
+
+    it 'should run_script with no arguments' do
+      tempfile = Tempfile.new('script.sh')
+      tempfile.write(@example_script)
+      tempfile.close
+
+      # Log file must be passes otherwise the Utility::Oss.run_script errors with
+      result = Utility::Oss.run_script(tempfile.path, 4.hours, {}, nil, Logger.new(STDOUT), @log_file)
+
+      expect(result).to eq true
+      expect(File.exist?(@log_file)).to eq true
+
+      result_log = File.read(@log_file).chomp
+      expect(result_log).to eq 'successfully ran'
+
+      tempfile.unlink
+    end
+
+    it 'should set only some env vars' do
+      ENV['BUNDLE_POINTLESS'] = 'Affirmative'
+      env_vars = Utility::Oss.resolve_env_vars({ 'my_custom_env' => 'set_to_this_value'})
+      expect(env_vars.has_key?('my_custom_env')).to eq true
+      expect(env_vars.has_key?('USER')).to eq false
+      expect(env_vars['BUNDLE_POINTLESS']).to eq 'Affirmative'
+    end
+  end
+end
