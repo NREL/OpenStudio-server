a754a4c5df8e9304b648bc6f1406c0902de667c6
Petersen, Anya <Anya.Petersen@nrel.gov>
2018-11-02 15:21:23 -0600

    Check for Oscli output in data point simulation log (should fail)

--- server/spec/features/dj_run_simulation_data_point_spec.rb
+++ server/spec/features/dj_run_simulation_data_point_spec.rb
@@ -166,6 +166,10 @@ RSpec.describe DjJobs::RunSimulateDataPoint, type: :feature, foreground: true do
     a = RestClient.get "http://#{host}/data_points/#{datapoint_id}.html"
     puts "accessed http://#{host}/data_points/#{datapoint_id}.html"
 
+    # pulling data point simulation log
+    l = RestClient.get "http://#{host}/data_points/#{datapoint_id}/download_result_file?filename=#{datapoint_id}.log"
+    expect(l.include? 'Oscli output:').to eq(true)
+
     # Verify that the results exist
     j = api.get_analysis_results(analysis_id)
     expect(j).to be_a Hash
