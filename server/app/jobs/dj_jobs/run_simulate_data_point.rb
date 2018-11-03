8ed1d4d0efab397b8c3fae15c1e12415224fc47b
Petersen, Anya <Anya.Petersen@nrel.gov>
2018-11-02 20:35:34 -0600

    add quotes around path.  sneaking into this pr because it's a pretty straightforward change.

--- server/app/jobs/dj_jobs/run_simulate_data_point.rb
+++ server/app/jobs/dj_jobs/run_simulate_data_point.rb
@@ -199,7 +199,7 @@ module DjJobs
             bundle = Rails.application.config.os_gemfile_path.present? ? "--bundle "\
             "#{File.join Rails.application.config.os_gemfile_path, 'Gemfile'} --bundle_path "\
             "#{File.join Rails.application.config.os_gemfile_path, 'gems'} " : ""
-            cmd = "#{@options[:openstudio_executable]} --verbose #{bundle}run --workflow #{osw_path} --debug"
+            cmd = "#{@options[:openstudio_executable]} --verbose #{bundle}run --workflow '#{osw_path}' --debug"
             process_log = File.join(simulation_dir, 'oscli_simulation.log')
             @sim_logger.info "Running workflow using cmd #{cmd} and writing log to #{process_log}"
 
