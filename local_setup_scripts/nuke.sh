c16737d1d3cf57e3f1fff1caf99ad4fbf2d87bea
brianlball <Brian.Ball@nrel.gov>
2019-05-07 09:12:09 -0600

    add r-notebook to local docker-compose.yml

--- local_setup_scripts/nuke.sh
+++ local_setup_scripts/nuke.sh
@@ -36,7 +36,7 @@ docker push 127.0.0.1:5000/mongo
 docker push 127.0.0.1:5000/redis
 
 echo "deploy"
-docker stack deploy osserver --compose-file=/home/ubuntu/docker-compose.yml &
+docker stack deploy osserver --compose-file=/home/ubuntu/Projects/OpenStudio-Server/local_setup_scripts/docker-compose.yml &
 wait $!
 while ( nc -zv 127.0.0.1 80 3>&1 1>&2- 2>&3- ) | awk -F ":" '$3 != " Connection refused" {exit 1}'; do sleep 5; done
 docker service scale osserver_worker=42
--- local_setup_scripts/rebuild_sr.sh
+++ local_setup_scripts/rebuild_sr.sh
@@ -21,7 +21,7 @@ docker pull redis:4.0.6
 docker tag redis:4.0.6 127.0.0.1:5000/redis
 docker push 127.0.0.1:5000/redis
 cd /home/ubuntu/
-docker stack deploy osserver --compose-file=/home/ubuntu/docker-compose.yml
+docker stack deploy osserver --compose-file=/home/ubuntu/Projects/OpenStudio-Server/local_setup_scripts/docker-compose.yml
 while ( nc -zv 127.0.0.1 80 3>&1 1>&2- 2>&3- ) | awk -F ":" '$3 != " Connection refused" {exit 1}'; do sleep 5; done
 docker service scale osserver_worker=42
 echo 'osserver stack rebuilt and redeployed'
