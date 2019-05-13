996ca7152543b86fd638d18aa31fb215bdaa7eb8
Brian Ball <brian.ball@nrel.gov>
2019-05-13 11:11:35 -0600

    add new notebooks in build and nuke

--- local_setup_scripts/nuke.sh
+++ local_setup_scripts/nuke.sh
@@ -17,6 +17,8 @@ docker pull nrel/openstudio-server:$1
 docker pull nrel/openstudio-rserve:$1
 docker pull mongo:3.4.10
 docker pull redis:4.0.6
+docker pull nrel/openstudio-jupyter:latest
+docker pull nrel/openstudio-rnotebook
 
 echo "create registry"
 docker volume create --name=regdata
@@ -28,17 +30,23 @@ docker tag nrel/openstudio-server:$1 127.0.0.1:5000/openstudio-server
 docker tag nrel/openstudio-rserve:$1 127.0.0.1:5000/openstudio-rserve
 docker tag mongo:3.4.10 127.0.0.1:5000/mongo
 docker tag redis:4.0.6 127.0.0.1:5000/redis
+docker tag nrel/openstudio-jupyter 127.0.0.1:5000/openstudio-jupyter
+docker tag nrel/openstudio-rnotebook 127.0.0.1:5000/openstudio-rnotebook
 sleep 3
 echo "cleanup"
 docker image rm mongo:3.4.10 -f
 docker image rm redis:4.0.6 -f
 docker image rm nrel/openstudio-server:$1 -f
 docker image rm nrel/openstudio-rserve:$1 -f
+docker image rm nrel/openstudio-jupyter:latest -f
+docker image rm nrel/openstudio-rnotebook:latest -f
 echo "push"
 docker push 127.0.0.1:5000/openstudio-server
 docker push 127.0.0.1:5000/openstudio-rserve
 docker push 127.0.0.1:5000/mongo
 docker push 127.0.0.1:5000/redis
+docker push 127.0.0.1:5000/openstudio-jupyter
+docker push 127.0.0.1:5000/openstudio-rnotebook
 
 echo "deploy"
 docker stack deploy osserver --compose-file=/home/ubuntu/Projects/OpenStudio-Server/local_setup_scripts/docker-compose.yml &
--- local_setup_scripts/rebuild_sr.sh
+++ local_setup_scripts/rebuild_sr.sh
@@ -6,20 +6,35 @@ docker volume rm osdata || true
 docker volume rm dbdata || true
 
 while [ $(docker ps -q | wc -l) != 1 ]; do sleep 5; done
+echo "server rebuild"
 docker image rm 127.0.0.1:5000/openstudio-server -f
 docker build . -t="127.0.0.1:5000/openstudio-server"
 docker push 127.0.0.1:5000/openstudio-server
+echo "rserve rebuild"
 cd /home/ubuntu/Projects/OpenStudio-Server/docker/R/
 docker image rm 127.0.0.1:5000/openstudio-rserve -f
 docker build . -t="127.0.0.1:5000/openstudio-rserve"
 docker push 127.0.0.1:5000/openstudio-rserve
+echo "jupyter rebuild"
+cd /home/ubuntu/Projects/docker-openstudio-jupyter
+docker image rm 127.0.0.1:5000/openstudio-jupyter
+docker build . -t="127.0.0.1:5000/openstudio-jupyter"
+docker push 127.0.0.1:5000/openstudio-jupyter
+echo "rnotebook rebuild"
+cd /home/ubuntu/Projects/docker-openstudio-rnotebook
+docker image rm 127.0.0.1:5000/openstudio-rnotebook
+docker build . -t="127.0.0.1:5000/openstudio-rnotebook"
+docker push 127.0.0.1:5000/openstudio-rnotebook
+echo "pull mongo"
 docker pull mongo:3.4.10
 docker tag mongo 127.0.0.1:5000/mongo
 docker push 127.0.0.1:5000/mongo
-docker image rm mongo || true
+docker image rm mongo -f || true
+echo "pull redis"
 docker pull redis:4.0.6
 docker tag redis:4.0.6 127.0.0.1:5000/redis
 docker push 127.0.0.1:5000/redis
+docker image rm redis -f || true
 cd /home/ubuntu/
 docker stack deploy osserver --compose-file=/home/ubuntu/Projects/OpenStudio-Server/local_setup_scripts/docker-compose.yml
 while ( nc -zv 127.0.0.1 80 3>&1 1>&2- 2>&3- ) | awk -F ":" '$3 != " Connection refused" {exit 1}'; do sleep 5; done
