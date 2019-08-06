ee77efdceac331a5ccae9205696d4cdbfc3857bb
Petersen, Anya <Anya.Petersen@nrel.gov>
2019-08-06 10:39:47 -0600

    for now, only testing install_gems on windows

--- ci/appveyor/setup.cmd
+++ ci/appveyor/setup.cmd
@@ -1,15 +1,17 @@
 set PATH=C:\Program Files\Git\mingw64\bin;C:\projects\openstudio\bin;%PATH%
-echo Downloading and Installing OpenStudio (%OPENSTUDIO_VERSION%%OPENSTUDIO_VERSION_EXT%.%OPENSTUDIO_VERSION_SHA%)
-curl -SLO --insecure https://openstudio-ci-builds.s3-us-west-2.amazonaws.com/develop3/OpenStudio-2.8.1.5f1c403208-Windows.exe
-OpenStudio-%OPENSTUDIO_VERSION%%OPENSTUDIO_VERSION_EXT%.%OPENSTUDIO_VERSION_SHA%-Windows.exe --script ci/appveyor/install-windows.qs
-move C:\openstudio C:\projects\openstudio
-dir C:\projects\openstudio
-dir C:\projects\openstudio\Ruby
+REM echo Downloading and Installing OpenStudio (%OPENSTUDIO_VERSION%%OPENSTUDIO_VERSION_EXT%.%OPENSTUDIO_VERSION_SHA%)
+REM curl -SLO --insecure https://openstudio-ci-builds.s3-us-west-2.amazonaws.com/develop3/OpenStudio-2.8.1.5f1c403208-Windows.exe
+REM OpenStudio-%OPENSTUDIO_VERSION%%OPENSTUDIO_VERSION_EXT%.%OPENSTUDIO_VERSION_SHA%-Windows.exe --script ci/appveyor/install-windows.qs
+REM move C:\openstudio C:\projects\openstudio
+REM dir C:\projects\openstudio
+REM dir C:\projects\openstudio\Ruby
 
 cd c:\projects\openstudio-server
 
 REM If you change RUBYLIB here, make sure to change it in integration-test.ps1 and unit-test.cmd too
 set RUBYLIB=C:\projects\openstudio\Ruby
+REM explicitly recompile psych for ruby 25 compatibility
+
 cd c:\
 REM Install rubygems-update in order to fix the SSL issue. Once updated, remove the gem.
 REM curl -SLO https://rubygems.org/downloads/rubygems-update-2.7.8.gem
