c20c9874b6b9e86ca40af79e6b51f18e6a11f585
Petersen, Anya <Anya.Petersen@nrel.gov>
2019-08-23 16:27:46 -0600

    test appveyor

--- ci/appveyor/setup.cmd
+++ ci/appveyor/setup.cmd
@@ -1,5 +1,6 @@
 set PATH=C:\Program Files\Git\mingw64\bin;C:\projects\openstudio\bin;%PATH%
-echo Downloading and Installing OpenStudio (%OPENSTUDIO_VERSION%%OPENSTUDIO_VERSION_EXT%.%OPENSTUDIO_VERSION_SHA%)
+echo Downloading and Installing OpenStudio (develop3 branch, %OPENSTUDIO_VERSION%%OPENSTUDIO_VERSION_EXT%.%OPENSTUDIO_VERSION_SHA%)
+REM install  develop3 build
 curl -SLO --insecure https://openstudio-ci-builds.s3-us-west-2.amazonaws.com/develop3/OpenStudio-2.8.1.5f1c403208-Windows.exe
 OpenStudio-%OPENSTUDIO_VERSION%%OPENSTUDIO_VERSION_EXT%.%OPENSTUDIO_VERSION_SHA%-Windows.exe --script ci/appveyor/install-windows.qs
 move C:\openstudio C:\projects\openstudio
