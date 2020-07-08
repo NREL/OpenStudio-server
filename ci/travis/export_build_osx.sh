eec55f32c2cfa7dd5ac1bb7f7ba44ee4f12f5619
Petersen, Anya <Anya.Petersen@nrel.gov>
2020-07-08 11:16:05 -0600

    Cleaner export PATH (which is reproduced in our S3 bucket).

--- ci/travis/export_build_osx.sh
+++ ci/travis/export_build_osx.sh
@@ -8,7 +8,14 @@ export GEM_HOME="$TRAVIS_BUILD_DIR/gems"
 export GEM_PATH="$TRAVIS_BUILD_DIR/gems:$TRAVIS_BUILD_DIR/gems/bundler/gems"
 ruby "${TRAVIS_BUILD_DIR}/bin/openstudio_meta" install_gems --export="/Users/travis/build/NREL/export"
 
-export ARTIFACTS_PATHS="$(ls /Users/travis/build/NREL/export/*.tar.gz | tr "\n" ":")"
+oss_filename="OpenStudio-server-${OPENSTUDIO_VERSION_SHA}-darwin.tar.gz"
+
+ls /Users/travis/build/NREL/export
+
+sudo mkdir /${OPENSTUDIO_VERSION}
+
+sudo mv /Users/travis/build/NREL/export/${oss_filename}  /${OPENSTUDIO_VERSION}/
+export ARTIFACTS_PATHS="/${OPENSTUDIO_VERSION}/${oss_filename}"
 # set to 2GB instead of default of 1
 export ARTIFACTS_MAX_SIZE=2147483648 
 # supposedly installed by default but not available
