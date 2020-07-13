3279788e2131264be64a21c4dc2f27b2b15019b5
Petersen, Anya <Anya.Petersen@nrel.gov>
2020-07-13 13:04:34 -0600

    Try again with ARTIFACTS_BUCKET updated in Travis repo settings.

--- ci/travis/export_build_osx.sh
+++ ci/travis/export_build_osx.sh
@@ -1,16 +1,15 @@
 #empty dir for export
 mkdir /Users/travis/build/NREL/export
 
-# must be set for gems (esp openstudio-workflow) to find openstudio when install_gems runs install_gems
 export PATH="/usr/local/ruby/bin:$PATH"
 export RUBYLIB="$HOME/openstudio/Ruby"
 export GEM_HOME="$TRAVIS_BUILD_DIR/gems"
 export GEM_PATH="$TRAVIS_BUILD_DIR/gems:$TRAVIS_BUILD_DIR/gems/bundler/gems"
 ruby "${TRAVIS_BUILD_DIR}/bin/openstudio_meta" install_gems --export="/Users/travis/build/NREL/export"
-
 oss_filename="OpenStudio-server-$(git -C "${TRAVIS_BUILD_DIR}" rev-parse --short=10 HEAD)-darwin.tar.gz"
 
-#mv build package to root of travis build for Artifact upload.
+
+#mv build package to root of travis build for artifact upload.
 mv /Users/travis/build/NREL/export/$oss_filename .
 
 export ARTIFACTS_PATHS=$oss_filename
