78f7ab3809103378c452fe7e192bddfb6500a7cc
Petersen, Anya <Anya.Petersen@nrel.gov>
2020-07-13 11:28:42 -0600

    Ok.  ARTIFACTS_PATHS value relative to Travis Build Dir.

--- ci/travis/export_build_osx.sh
+++ ci/travis/export_build_osx.sh
@@ -8,9 +8,12 @@ export GEM_HOME="$TRAVIS_BUILD_DIR/gems"
 export GEM_PATH="$TRAVIS_BUILD_DIR/gems:$TRAVIS_BUILD_DIR/gems/bundler/gems"
 ruby "${TRAVIS_BUILD_DIR}/bin/openstudio_meta" install_gems --export="/Users/travis/build/NREL/export"
 
-cd /Users/travis/build/NREL/export
+oss_filename="OpenStudio-server-${OPENSTUDIO_VERSION_SHA}-darwin.tar.gz"
 
-export ARTIFACTS_PATHS="OpenStudio-server-${OPENSTUDIO_VERSION_SHA}-darwin.tar.gz"
+#mv build package to root of travis build for Artifact upload.
+mv /Users/travis/build/NREL/export/$oss_filename .
+
+export ARTIFACTS_PATHS=$oss_filename
 # set to 2GB instead of default of 1
 export ARTIFACTS_MAX_SIZE=2147483648 
 # supposedly installed by default but not available
