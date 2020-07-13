dac06c17c763647631ae3e427c87d3e826b323c8
Petersen, Anya <Anya.Petersen@nrel.gov>
2020-07-13 12:07:01 -0600

    OSS SHA rather than OS SHA.

--- ci/travis/export_build_osx.sh
+++ ci/travis/export_build_osx.sh
@@ -8,7 +8,7 @@ export GEM_HOME="$TRAVIS_BUILD_DIR/gems"
 export GEM_PATH="$TRAVIS_BUILD_DIR/gems:$TRAVIS_BUILD_DIR/gems/bundler/gems"
 ruby "${TRAVIS_BUILD_DIR}/bin/openstudio_meta" install_gems --export="/Users/travis/build/NREL/export"
 
-oss_filename="OpenStudio-server-${OPENSTUDIO_VERSION_SHA}-darwin.tar.gz"
+oss_filename="OpenStudio-server-$(git -C "${TRAVIS_BUILD_DIR}" rev-parse --short=10 HEAD)-darwin.tar.gz"
 
 #mv build package to root of travis build for Artifact upload.
 mv /Users/travis/build/NREL/export/$oss_filename .
