7e9e4c5ac436b6f7695dc4e5b06c981443d15e57
Nicholas Long <nicholas.long@nrel.gov>
2020-07-13 21:41:46 -0600

    add tests for osa schema.

---
+++ server/spec/models/schema_validation_spec.rb
@@ -0,0 +1,86 @@
+# *******************************************************************************
+# OpenStudio(R), Copyright (c) 2008-2020, Alliance for Sustainable Energy, LLC.
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# (1) Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# (2) Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# (3) Neither the name of the copyright holder nor the names of any contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission from the respective party.
+#
+# (4) Other than as required in clauses (1) and (2), distributions in any form
+# of modifications or other derivative works may not use the "OpenStudio"
+# trademark, "OS", "os", or any other confusingly similar designation without
+# specific prior written permission from Alliance for Sustainable Energy, LLC.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER, THE UNITED STATES
+# GOVERNMENT, OR ANY CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# *******************************************************************************
+
+require 'rails_helper'
+require 'json-schema'
+
+def get_osa(path)
+  osa = nil
+  osa_path = File.expand_path("../../../#{path}", __dir__)
+  puts osa_path
+
+  expect(File.exist?(osa_path)).to be true
+  File.open(osa_path) do |f|
+    osa = JSON.parse(f.read, symbolize_names: true)
+  end
+  expect(osa).to_not be_nil
+
+  osa
+end
+
+def validate_osa(path, schema)
+  osa = get_osa(path)
+
+  puts "**** Checking validity of OSA: #{path} *****"
+
+  errors = JSON::Validator.fully_validate(schema, osa)
+  expect(errors.empty?).to eq(true), "OSA '#{path}' is not valid, #{errors}"
+end
+
+RSpec.describe 'OSA Schema' do
+  before :all do
+    @schema = nil
+    schema_path = File.expand_path('../../app/lib/analysis_library/schema/osa.json', __dir__)
+    expect(File.exist?(schema_path)).to be true
+    File.open(schema_path) do |f|
+      @schema = JSON.parse(f.read, symbolize_names: true)
+    end
+    expect(@schema).to_not be_nil
+  end
+
+  it 'should be a valid osa file' do
+    validate_osa('server/spec/files/batch_datapoints/example_csv.json', @schema)
+    validate_osa('server/spec/files/batch_datapoints/example_csv_with_scripts.json', @schema)
+    validate_osa('server/spec/files/batch_datapoints/the_project.json', @schema)
+    validate_osa('server/spec/files/jsons/sweep_smalloffice.json', @schema)
+    validate_osa('server/spec/files/test_model/test_model.json', @schema)
+    validate_osa('spec/files/da_measures.json', @schema)
+    validate_osa('spec/files/example_csv.json', @schema)
+
+    # fails -- but not used
+    # validate_osa('server/spec/files/simple_cont_example.json', @schema)
+    # validate_osa('server/spec/files/jsons/formulation.json', @schema)
+  end
+end
