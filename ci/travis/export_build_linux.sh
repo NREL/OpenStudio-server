bcf628a17cffd7b898235caaf64e2906fede9a69
Petersen, Anya <Anya.Petersen@nrel.gov>
2020-07-14 21:21:25 -0600

    more with linux builds

--- ci/travis/export_build_linux.sh
+++ ci/travis/export_build_linux.sh
@@ -8,10 +8,12 @@ export GEM_PATH="$TRAVIS_BUILD_DIR/gems:$TRAVIS_BUILD_DIR/gems/bundler/gems"
 export RUBYLIB="/usr/local/openstudio-${OPENSTUDIO_VERSION}/Ruby"
 export OPENSTUDIO_TEST_EXE="/usr/local/openstudio-${OPENSTUDIO_VERSION}/bin/openstudio"
 mongo_dir="/usr/bin"
-
-
 /usr/local/ruby/bin/ruby "${TRAVIS_BUILD_DIR}/bin/openstudio_meta" install_gems --export="/home/travis/build/NREL/export"
 oss_filename="OpenStudio-server-$(git -C "${TRAVIS_BUILD_DIR}" rev-parse --short=10 HEAD)-linux.tar.gz"
+ls -al /Users/travis/build/NREL/export/
+#mv build package to root of travis build for artifact upload.
+mv /Users/travis/build/NREL/export/$oss_filename .
+ls -al
 
 export ARTIFACTS_PATHS=$oss_filename
 # set to 2GB instead of default of 1
