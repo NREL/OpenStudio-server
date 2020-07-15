2c78583704576dc381cd1c573cd9b6916be6f017
Petersen, Anya <Anya.Petersen@nrel.gov>
2020-07-14 20:59:24 -0600

    Try Linux publishing.

--- ci/travis/export_build_linux.sh
+++ ci/travis/export_build_linux.sh
@@ -1,16 +1,19 @@
-export PATH="/usr/local/openstudio-${OPENSTUDIO_VERSION}/bin:${PATH}"
-export GEM_HOME="$TRAVIS_BUILD_DIR/gems"
-export GEM_PATH="$TRAVIS_BUILD_DIR/gems:$TRAVIS_BUILD_DIR/gems/bundler/gems"
-# Dir containing openstudio
-export RUBYLIB="/usr/local/openstudio-${OPENSTUDIO_VERSION}/Ruby:/usr/Ruby"
-export OPENSTUDIO_TEST_EXE="/usr/local/openstudio-${OPENSTUDIO_VERSION}/bin/openstudio"
-mongo_dir="/usr/bin"
 # empty dir for export
 mkdir /home/travis/build/NREL/export
 
-ruby "${TRAVIS_BUILD_DIR}/bin/openstudio_meta" install_gems --export="/home/travis/build/NREL/export"
+export PATH="/usr/loca/bin/ruby:/usr/local/openstudio-${OPENSTUDIO_VERSION}/bin:${PATH}"
+export GEM_HOME="$TRAVIS_BUILD_DIR/gems"
+export GEM_PATH="$TRAVIS_BUILD_DIR/gems:$TRAVIS_BUILD_DIR/gems/bundler/gems"
+# Dir containing openstudio
+export RUBYLIB="/usr/local/openstudio-${OPENSTUDIO_VERSION}/Ruby"
+export OPENSTUDIO_TEST_EXE="/usr/local/openstudio-${OPENSTUDIO_VERSION}/bin/openstudio"
+mongo_dir="/usr/bin"
 
-export ARTIFACTS_PATHS="$(ls /home/travis/build/NREL/export/*.tar.gz | tr "\n" ":")"
+
+/usr/local/ruby/bin/ruby "${TRAVIS_BUILD_DIR}/bin/openstudio_meta" install_gems --export="/home/travis/build/NREL/export"
+oss_filename="OpenStudio-server-$(git -C "${TRAVIS_BUILD_DIR}" rev-parse --short=10 HEAD)-linux.tar.gz"
+
+export ARTIFACTS_PATHS=$oss_filename
 # set to 2GB instead of default of 1
 export ARTIFACTS_MAX_SIZE=2147483648 
 # supposedly installed by default but not available
