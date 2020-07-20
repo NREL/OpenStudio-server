c132ba2fbd1a85b521c1da8acf528695474e629d
Petersen, Anya <Anya.Petersen@nrel.gov>
2020-07-20 11:00:07 -0600

    test osx publishing.

--- ci/travis/export_build_osx.sh
+++ ci/travis/export_build_osx.sh
@@ -5,7 +5,7 @@ export PATH="/usr/local/ruby/bin:$PATH"
 export RUBYLIB="$HOME/openstudio/Ruby"
 export GEM_HOME="$TRAVIS_BUILD_DIR/gems"
 export GEM_PATH="$TRAVIS_BUILD_DIR/gems:$TRAVIS_BUILD_DIR/gems/bundler/gems"
-ruby "${TRAVIS_BUILD_DIR}/bin/openstudio_meta" install_gems --export="/Users/travis/build/NREL/export"
+/usr/local/ruby/bin/ruby "${TRAVIS_BUILD_DIR}/bin/openstudio_meta" install_gems --export="/Users/travis/build/NREL/export"
 oss_filename="OpenStudio-server-$(git -C "${TRAVIS_BUILD_DIR}" rev-parse --short=10 HEAD)-darwin.tar.gz"
 
 
