8b805fb6bd9c12cdd8c39cdf57d17d145d7717a9
Anya Petersen <anyaelena@gmail.com>
2020-09-25 10:23:26 -0600

    try to build and upload package.

---
+++ ci/appveyor/build_package.cmd
@@ -0,0 +1,11 @@
+set PATH=C:\projects\ruby\bin;C:\Program Files\Git\mingw64\bin;C:\projects\openstudio\bin;%PATH%
+set GEM_HOME=C:\projects\openstudio-server\gems
+set GEM_PATH=C:\projects\openstudio-server\gems;C:\projects\openstudio-server\gems\gems\bundler\gems
+set RUBYLIB=C:\projects\openstudio\Ruby
+set OPENSTUDIO_TEST_EXE=C:\projects\openstudio\bin\openstudio
+REM set mongo_dir??
+cd c:\
+mkdir export
+ruby C:\projects\openstudio-server\bin\openstudio_meta install_gems --export="C:\export"
+mv C:\export C:\projects\openstudio-server\export
+dir C:\projects\openstudio-server\export
