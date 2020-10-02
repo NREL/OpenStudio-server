31bf9397b52b15d6b01177ff6d1127171f7b6b81
Anya Petersen <anyaelena@gmail.com>
2020-10-02 08:57:17 -0600

    Paths should include OPENSTUDIO_VERSION_EXT.

--- ci/travis/export_build_linux.sh
+++ ci/travis/export_build_linux.sh
@@ -1,12 +1,12 @@
 # empty dir for export
 mkdir /home/travis/build/NREL/export
 
-export PATH="/usr/loca/bin/ruby:/usr/local/openstudio-${OPENSTUDIO_VERSION}/bin:${PATH}"
+export PATH="/usr/loca/bin/ruby:/usr/local/openstudio-${OPENSTUDIO_VERSION}${OPENSTUDIO_VERSION_EXT}/bin:${PATH}"
 export GEM_HOME="$TRAVIS_BUILD_DIR/gems"
 export GEM_PATH="$TRAVIS_BUILD_DIR/gems:$TRAVIS_BUILD_DIR/gems/bundler/gems"
 # Dir containing openstudio
-export RUBYLIB="/usr/local/openstudio-${OPENSTUDIO_VERSION}/Ruby"
-export OPENSTUDIO_TEST_EXE="/usr/local/openstudio-${OPENSTUDIO_VERSION}/bin/openstudio"
+export RUBYLIB="/usr/local/openstudio-${OPENSTUDIO_VERSION}${OPENSTUDIO_VERSION_EXT}/Ruby"
+export OPENSTUDIO_TEST_EXE="/usr/local/openstudio-${OPENSTUDIO_VERSION}${OPENSTUDIO_VERSION_EXT}/bin/openstudio"
 mongo_dir="/usr/bin"
 /usr/local/ruby/bin/ruby "${TRAVIS_BUILD_DIR}/bin/openstudio_meta" install_gems --export="/home/travis/build/NREL/export"
 oss_filename="OpenStudio-server-$(git -C "${TRAVIS_BUILD_DIR}" rev-parse --short=10 HEAD)-linux.tar.gz"
@@ -20,7 +20,6 @@ export ARTIFACTS_PATHS=$oss_filename
 export ARTIFACTS_MAX_SIZE=2147483648 
 # supposedly installed by default but not available
 curl -sL https://raw.githubusercontent.com/travis-ci/artifacts/master/install | bash
-# TODO don't recreate entire path to file within the AWS bucket.
 artifacts upload
 exit $?
 
