55f01aef3018546c77da57f0a77edcb7763f4346
tijcolem <tjc.coleman@gmail.com>
2021-01-15 11:16:11 -0700

    Use one job to string together the build, unit, integration and finally uploading gem packages. This should save some time with the uploading as we don't have to call the setup.sh again

--- ci/travis/export_build_osx.sh
+++ ci/travis/export_build_osx.sh
@@ -1,22 +1,24 @@
 #empty dir for export
-mkdir /Users/travis/build/NREL/export
+mkdir -p $GITHUB_WORKSPACE/build/NREL/export
 
-export PATH="/usr/local/ruby/bin:$PATH"
-export RUBYLIB="$HOME/openstudio/Ruby"
+
+export OS_NAME_WITH_PLUS=OpenStudio-${OPENSTUDIO_VERSION}${OPENSTUDIO_VERSION_EXT}+${OPENSTUDIO_VERSION_SHA}-Darwin
+export PATH="$GITHUB_WORKSPACE/gems/bin:/usr/local/ruby/bin:$HOME/$OS_NAME_WITH_PLUS/bin:$PATH"
+export RUBYLIB="$HOME/$OS_NAME_WITH_PLUS/Ruby"
 export GEM_HOME="$TRAVIS_BUILD_DIR/gems"
 export GEM_PATH="$TRAVIS_BUILD_DIR/gems:$TRAVIS_BUILD_DIR/gems/bundler/gems"
-/usr/local/ruby/bin/ruby "${TRAVIS_BUILD_DIR}/bin/openstudio_meta" install_gems --export="/Users/travis/build/NREL/export"
-oss_filename="OpenStudio-server-$(git -C "${TRAVIS_BUILD_DIR}" rev-parse --short=10 HEAD)-darwin.tar.gz"
+/usr/local/ruby/bin/ruby "${GITHUB_WORKSPACE}/bin/openstudio_meta" install_gems --export="${GITHUB_WORKSPACE}/build/NREL/export"
+#oss_filename="OpenStudio-server-$(git -C "${GITHUB_WORKSPACE}" rev-parse --short=10 HEAD)-darwin.tar.gz"
 
 
 #mv build package to root of travis build for artifact upload.
-mv /Users/travis/build/NREL/export/$oss_filename .
+#mv $HOME/build/NREL/export/$oss_filename .
 
-export ARTIFACTS_PATHS=$oss_filename
+#export ARTIFACTS_PATHS=$oss_filename
 # set to 2GB instead of default of 1
-export ARTIFACTS_MAX_SIZE=2147483648 
+#export ARTIFACTS_MAX_SIZE=2147483648 
 # supposedly installed by default but not available
-curl -sL https://raw.githubusercontent.com/travis-ci/artifacts/master/install | bash
+#curl -sL https://raw.githubusercontent.com/travis-ci/artifacts/master/install | bash
 # TODO don't recreate entire path to file within the AWS bucket.
-artifacts upload
-exit $?
+#artifacts upload
+#exit $?
