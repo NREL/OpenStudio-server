b8b1d62c25d910caa5f92ebd762461c5d1cb399c
brianlball <Brian.Ball@nrel.gov>
2021-06-28 16:46:45 -0600

    change failure to upper case so test fails

--- server/app/jobs/dj_jobs/run_simulate_data_point.rb
+++ server/app/jobs/dj_jobs/run_simulate_data_point.rb
@@ -267,7 +267,7 @@ module DjJobs
             if (!process_log.nil? && File.exist?(process_log))
               @sim_logger.info "Oscli output: #{File.read(process_log)}"
             else
-              @sim_logger.warn "Oscli output: #{process_log} does not exist"
+              @sim_logger.warn "OSCLI output: #{process_log} does not exist"
             end
             #docker_log = File.join(APP_CONFIG['rails_log_path'], 'docker.log')
             #if File.exist? docker_log
