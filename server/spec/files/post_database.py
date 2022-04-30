68a300a5b9fb01cd380e44680e0efc20da0a65fe
brianlball <brian.ball@nrel.gov>
2022-04-30 12:54:15 -0400

    remove python restapi file.  making fullstack_spec in pareto branch

--- server/spec/files/post_database.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-URL = 'http://127.0.0.1/'
-f = open('/opt/openstudio/server/spec/files/mongodump_1651085721.tar.gz', 'rb')
-files = {"file": ("mongodump_1651085721.tar.gz", f)}
-r = requests.post(url=URL+'admin/restore_database', files=files)
-print(r.json)
-
