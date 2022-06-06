45e8eb6e9fa901495326e3f01b039d2ccf37425f
brianlball <brian.ball@nrel.gov>
2022-06-06 12:08:13 -0400

    add another test for spamming analyses and nginx reloading

--- server/spec/features/docker_stack_test_nginx_spec.rb
+++ server/spec/features/docker_stack_test_nginx_spec.rb
@@ -140,5 +140,210 @@ RSpec.describe 'TestAPIs', type: :feature do
     puts 'expect \'Active connections\' in body'
     compare = a.body.include?('Active connections')
     expect(compare).to be true
+    
+    sleep(1)
+    puts 'remove any existing projects'
+    a = RestClient.get "http://#{@host}/projects.json"
+    a = JSON.parse(a, symbolize_names: true)
+    a.each do |project|
+        sleep(1)
+        id = project[:_id]
+        puts "removing existing project id: #{id}"
+        begin
+            RestClient.delete "http://#{@host}/projects/#{id}"
+        rescue RestClient::ExceptionWithResponse => err
+            case err.http_code
+            when 301, 302, 307
+                puts '   redirecting after delete'
+            end
+        end
+    end
+    
+    # the tests below submit an analysis and once that queues datapoints
+    # it submits another analysis, which will reload nginx while the other
+    # analysis is running.  the test expects both analysis to complete all 
+    # six datapoints that are successful with objective function values
+    
+    sleep(1)
+    puts 'check there are no projects'
+    a = RestClient.get "http://#{@host}/projects.json"
+    a = JSON.parse(a, symbolize_names: true)
+    expect(a).to be_empty
+    
+    sleep(1)
+    puts 'check that there are no analyses'
+    a = RestClient.get "http://#{@host}/analyses.json"
+    a = JSON.parse(a, symbolize_names: true)
+    expect(a).to be_empty
+    
+    sleep(1)
+    puts 'run a morris analysis'
+    # run an analysis
+    command = "#{@bundle_cmd} #{@meta_cli} run_analysis --debug --verbose '#{@project}/SEB_Morris_2013.json' 'http://#{@host}' -z 'SEB_calibration_NSGA_2013' -a morris"
+    puts "run command: #{command}"
+    run_analysis = system(command)
+    expect(run_analysis).to be true
+    
+    sleep(3)
+    a = RestClient.get "http://#{@host}/analyses.json"
+    a = JSON.parse(a, symbolize_names: true)
+    a = a.sort { |x, y| x[:created_at] <=> y[:created_at] }.reverse
+    expect(a).not_to be_empty
+    analysis = a[0]
+    analysis_id = analysis[:_id]
+    
+    a = RestClient.get "http://#{@host}/analyses/#{analysis_id}/status.json"
+    a = JSON.parse(a, symbolize_names: true)
+    analysis_type = a[:analysis][:analysis_type]
+    expect(analysis_type).to eq('morris')
+
+    status = a[:analysis][:status]
+    expect(status).not_to be_nil
+    puts "Accessed pages for analysis: #{analysis_id}, analysis_type: #{analysis_type}, status: #{status}"
+    
+    puts "sleep for 10 seconds"
+    sleep(10)
+    get_count = 0
+    get_count_max = 50
+    # get all data points in this analysis
+    puts 'make sure datapoints are queued or started'
+    begin
+        a = RestClient.get "http://#{@host}/data_points.json"
+        a = JSON.parse(a, symbolize_names: true)
+        data_points = []
+        a.each do |data_point|
+          if data_point[:analysis_id] == analysis_id
+            data_points << data_point
+          end
+        end
+        puts "datapoints: #{data_points}"
+        have_datapoints = !data_points.empty?
+        puts "have_datapoints: #{have_datapoints}"
+        raise if have_datapoints != true
+        # confirm that queueing is working
+        data_points.each do |data_point|
+          # get the datapoint pages
+          data_point_id = data_point[:_id]
+          expect(data_point_id).not_to be_nil
+
+          a = RestClient.get "http://#{@host}/data_points/#{data_point_id}.json"
+          a = JSON.parse(a, symbolize_names: true)
+          expect(a).not_to be_nil
+
+          data_points_status = a[:data_point][:status]
+          expect(data_points_status).not_to be_nil
+          puts "Accessed pages for data_point #{data_point_id}, data_points_status = #{data_points_status}"
+        end
+    rescue => e
+        puts "rescue: #{e} get_count: #{get_count}"
+        sleep(10)
+        retry if get_count <= get_count_max
+        expect(have_datapoints).to be true
+    end
+    
+    puts "have datapoints queued, submit another analysis"
+    #submit another job with datapoint started and queued
+    #this will reload nginx
+    # run an analysis
+    command = "#{@bundle_cmd} #{@meta_cli} run_analysis --debug --verbose '#{@project}/SEB_Morris_2013.json' 'http://#{@host}' -z 'SEB_calibration_NSGA_2013' -a morris"
+    puts "run command: #{command}"
+    run_analysis = system(command)
+    expect(run_analysis).to be true
+    
+    sleep(3)
+    a = RestClient.get "http://#{@host}/analyses.json"
+    a = JSON.parse(a, symbolize_names: true)
+    a = a.sort { |x, y| x[:created_at] <=> y[:created_at] }.reverse
+    expect(a).not_to be_empty
+    analysis = a[0]
+    new_analysis_id = analysis[:_id]
+    
+    #wait till first analysis completes
+    puts "run second analysis"
+    status = 'queued'
+    timeout_seconds = 360
+    begin
+      ::Timeout.timeout(timeout_seconds) do
+        while status != 'completed'
+          # get the analysis pages
+          get_count = 0
+          get_count_max = 50
+          begin
+            a = RestClient.get "http://#{@host}/analyses/#{analysis_id}/status.json"
+            a = JSON.parse(a, symbolize_names: true)
+
+            status = a[:analysis][:status]
+            expect(status).not_to be_nil
+            puts "Accessed pages for analysis: #{analysis_id}, analysis_type: #{analysis_type}, status: #{status}"
+
+          rescue RestClient::ExceptionWithResponse => e
+            puts "rescue: #{e} get_count: #{get_count}"
+            sleep(10.0)
+            retry if get_count <= get_count_max
+          end
+          puts ''
+          sleep 10
+        end
+      end
+    rescue ::Timeout::Error
+      puts "Analysis status is `#{status}` after #{timeout_seconds} seconds; assuming error."
+    end
+    expect(status).to eq('completed')
+
+    puts "wait for second analysis to complete"
+    #wait till second analysis completes
+    status = 'queued'
+    timeout_seconds = 360
+    begin
+      ::Timeout.timeout(timeout_seconds) do
+        while status != 'completed'
+          # get the analysis pages
+          get_count = 0
+          get_count_max = 50
+          begin
+            a = RestClient.get "http://#{@host}/analyses/#{new_analysis_id}/status.json"
+            a = JSON.parse(a, symbolize_names: true)
+
+            status = a[:analysis][:status]
+            expect(status).not_to be_nil
+            puts "Accessed pages for analysis: #{new_analysis_id}, analysis_type: #{analysis_type}, status: #{status}"
+
+          rescue RestClient::ExceptionWithResponse => e
+            puts "rescue: #{e} get_count: #{get_count}"
+            sleep(10.0)
+            retry if get_count <= get_count_max
+          end
+          puts ''
+          sleep 10
+        end
+      end
+    rescue ::Timeout::Error
+      puts "Analysis status is `#{status}` after #{timeout_seconds} seconds; assuming error."
+    end
+    expect(status).to eq('completed')
+    
+    puts "expect there to be 6 datapoints"
+    dps = RestClient.get "http://#{@host}/data_points.json"
+    dps = JSON.parse(dps, symbolize_names: true)
+    expect(dps).not_to be_nil
+    expect(dps.size).to eq(6)
+    
+    puts "confirm that all datapoints ran successfully and have objective function values"
+    dps.each do |data_point|
+        dp = RestClient.get "http://#{@host}/data_points/#{data_point[:_id]}.json"
+        dp = JSON.parse(dp, symbolize_names: true)
+        expect(dp[:data_point][:status_message]).to eq('completed normal')
+
+        results = dp[:data_point][:results][:calibration_reports_enhanced_20]
+        expect(results).not_to be_nil
+        sim = results.slice(:electricity_consumption_cvrmse, :electricity_consumption_nmbe, :natural_gas_consumption_cvrmse, :natural_gas_consumption_nmbe)
+        expect(sim.size).to eq(4)
+        sim = sim.transform_values { |x| x.truncate(4) }
+        sim.values.each do |s|
+            is_a_number = s.is_a?(Float)
+            expect(is_a_number).to be true
+        end        
+    end
+      
   end    
 end
