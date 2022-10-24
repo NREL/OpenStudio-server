3e2807ea074dc5c611e2f0fcfe80ef10ad5a992d
Tim Coleman <tjc.coleman@gmail.com>
2022-10-24 15:53:56 -0600

    few changes fo 3.5.0-rc1 paths

--- ci/github-actions/setup.sh
+++ ci/github-actions/setup.sh
@@ -34,10 +34,9 @@ else
         rm -r mongodb-macos*
 
         # Install openstudio -- Use the install script that is in this repo now, the one on OpenStudio/develop has changed
-        export OS_NAME=OpenStudio-${OPENSTUDIO_VERSION}${OPENSTUDIO_VERSION_EXT}%2B${OPENSTUDIO_VERSION_SHA}-Darwin
-        export OS_NAME_WITH_PLUS=OpenStudio-${OPENSTUDIO_VERSION}${OPENSTUDIO_VERSION_EXT}+${OPENSTUDIO_VERSION_SHA}-Darwin
+        export OS_NAME=OpenStudio-${OPENSTUDIO_VERSION}${OPENSTUDIO_VERSION_EXT}%2B${OPENSTUDIO_VERSION_SHA}-Darwin-x86_64
+        export OS_NAME_WITH_PLUS=OpenStudio-${OPENSTUDIO_VERSION}${OPENSTUDIO_VERSION_EXT}+${OPENSTUDIO_VERSION_SHA}-Darwin-x86_64
         curl -SL --insecure https://github.com/NREL/OpenStudio/releases/download/v3.5.0-rc1/${OS_NAME}.tar.gz -o $OS_NAME_WITH_PLUS.tar.gz
-        #curl -SL --insecure https://openstudio-ci-builds.s3-us-west-2.amazonaws.com/3.4.0-rc2/${OS_NAME}.tar.gz -o $OS_NAME_WITH_PLUS.tar.gz
         # OSX downloads with %2B but installs with + sign. These are the encoded chars in url strings.
         #hdiutil attach ${OS_NAME}.dmg
         #sed -i -e "s|REPLACEME|$HOME/openstudio|" ci/github-actions/install-mac.qs
