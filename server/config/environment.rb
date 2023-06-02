ff50d26d940e26e4bf2c5feeb44da80d71cc0788
kflemin <2205659+kflemin@users.noreply.github.com>
2023-06-02 14:52:11 -0600

    update licenses

--- server/config/environment.rb
+++ server/config/environment.rb
@@ -1,3 +1,8 @@
+# *******************************************************************************
+# OpenStudio(R), Copyright (c) Alliance for Sustainable Energy, LLC.
+# See also https://openstudio.net/license
+# *******************************************************************************
+
 # Load the Rails application.
 require File.expand_path('application', __dir__)
 
--- server/config/initializers/session_store.rb
+++ server/config/initializers/session_store.rb
@@ -1,3 +1,8 @@
+# *******************************************************************************
+# OpenStudio(R), Copyright (c) Alliance for Sustainable Energy, LLC.
+# See also https://openstudio.net/license
+# *******************************************************************************
+
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_openstudio_server_session'
