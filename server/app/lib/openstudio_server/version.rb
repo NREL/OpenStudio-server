659a61a525c5c9c2d52055ee39d3cbb5af068af5
wenyikuang <137332549+wenyikuang@users.noreply.github.com>
2023-11-22 17:11:28 -0700

    Revert "3.7.0-rc3 (#724)"
    This reverts commit 441c1aec347c7270008d3539a7f6a353974613dc.

--- server/app/lib/openstudio_server/version.rb
+++ server/app/lib/openstudio_server/version.rb
@@ -7,6 +7,6 @@ module OpenstudioServer
   VERSION = '3.7.0'.freeze
   # format should be ^.*\-{1}[a-z]+[0-9]+
   # for example: -rc1, -beta6, -customusecase0
-  VERSION_EXT = '-rc3'.freeze # with preceding - or +
-  OS_SHA = '37087fd612'.freeze
+  VERSION_EXT = '-rc1'.freeze # with preceding - or +
+  OS_SHA = '211bb633b0'.freeze
 end
