59b1fe494ae2da31f2978965f9d385820a5fae58
Brian Ball <brian.ball@nrel.gov>
2024-03-22 13:31:51 -0400

    try getting all .log

--- ci/appveyor/print_logs.cmd
+++ ci/appveyor/print_logs.cmd
@@ -8,19 +8,28 @@ echo.
 echo === PRINTING ERROR LOG REPORTS ===
 echo.
 
-for %%i in (C:\projects\openstudio-server\spec\files\logs\*.log) do (
+:: for %%i in (C:\projects\openstudio-server\spec\files\logs\*.log) do (
+::   echo ======================================================
+::    echo %%i
+::    echo ======================================================
+::    type %%i
+::    echo.
+::)
+
+::for %%i in (C:\projects\openstudio-server\spec\unit-test\logs\*.log) do (
+::    echo ======================================================
+::    echo %%i
+::    echo ======================================================
+::    type %%i
+::    echo.
+::)
+
+:: Single command to recursively search for and print all .log files
+:: starting from the current directory (spec)
+for /r %%i in (*.log) do (
     echo ======================================================
     echo %%i
     echo ======================================================
-    type %%i
+    type "%%i"
     echo.
 )
-
-for %%i in (C:\projects\openstudio-server\spec\unit-test\logs\*.log) do (
-    echo ======================================================
-    echo %%i
-    echo ======================================================
-    type %%i
-    echo.
-)
-
