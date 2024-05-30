7f54135b2008a1c0d327c51cd1a7ba7019d95582
Brian Ball <brian.ball@nrel.gov>
2024-05-30 11:45:54 -0400

    Update openstudio_meta_spec.rb
    --debug --verbose for helping with tracing 3.8.0-rc3 errors

--- spec/tests/openstudio_meta_spec.rb
+++ spec/tests/openstudio_meta_spec.rb
@@ -80,7 +80,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../server/Gemfile', __dir__)
 RSpec.describe OpenStudioMeta do
   before :all do
     # start the server
-    command = "#{ruby_cmd} \"#{meta_cli}\" start_local --mongo-dir=\"#{File.dirname(mongod_exe)}\" --openstudio-exe-path=#{ENV['OPENSTUDIO_TEST_EXE']} --worker-number=#{num_workers} \"#{project}\""
+    command = "#{ruby_cmd} \"#{meta_cli}\" start_local --debug --verbose --mongo-dir=\"#{File.dirname(mongod_exe)}\" --openstudio-exe-path=#{ENV['OPENSTUDIO_TEST_EXE']} --worker-number=#{num_workers} \"#{project}\""
     puts command
     start_local = system(command)
     expect(start_local).to be true
