871f0899647915bb8804dd34ffe8223b4d116ccb
brianlball <brian.ball@nrel.gov>
2024-11-04 07:19:32 -0600

    OS 3.9.0-rc1

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,8 @@
 # NOTES:            Currently this is one big dockerfile and non-optimal.
 
 #may include suffix
-ARG OPENSTUDIO_VERSION=3.8.0
-FROM nrel/openstudio:3.8.0 as base
+ARG OPENSTUDIO_VERSION=3.9.0
+FROM nrel/openstudio:dev-3.9.0-rc1 as base
 MAINTAINER Nicholas Long nicholas.long@nrel.gov
 
 ENV DEBIAN_FRONTEND=noninteractive
--- server/app/lib/openstudio_server/version.rb
+++ server/app/lib/openstudio_server/version.rb
@@ -4,9 +4,9 @@
 # *******************************************************************************
 
 module OpenstudioServer
-  VERSION = '3.8.0'.freeze
+  VERSION = '3.9.0'.freeze
   # format should be ^.*\-{1}[a-z]+[0-9]+
   # for example: -rc1, -beta6, -customusecase0
-  VERSION_EXT = '-rc3'.freeze # with preceding - or +
-  OS_SHA = 'd8eac889d0'.freeze
+  VERSION_EXT = '-rc2'.freeze # with preceding - or +
+  OS_SHA = 'cc1e0bbd6d'.freeze
 end
