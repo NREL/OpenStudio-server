4cff8f01dd8f7fc991931ec70564ba6e06e2907e
brianlball <brian.ball@nrel.gov>
2024-11-13 06:52:25 -0600

    release container to dockerhub

--- docker/deployment/scripts/deploy_docker_github_actions.sh
+++ docker/deployment/scripts/deploy_docker_github_actions.sh
@@ -14,8 +14,8 @@ elif [ "${GITHUB_REF}" == "refs/heads/master" ]; then
 # Currently setting this to setup_github_actions to test upload. 
 elif [ "${GITHUB_REF}" == "refs/heads/setup_github_actions" ]; then
     IMAGETAG=experimental
-elif [ "${GITHUB_REF}" == "refs/heads/3.9.0-rc1" ]; then
-     IMAGETAG="3.9.0-rc2"
+elif [ "${GITHUB_REF}" == "refs/heads/3.9.0-rc3" ]; then
+     IMAGETAG="3.9.0-rc3"
 fi
 
 if [ "${IMAGETAG}" != "skip" ]; then
