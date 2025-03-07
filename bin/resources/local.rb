f5a3480865620a5a0b434975f6b30d0ce8d3a3dc
Wenyi Kuang <wkuang@nrel.gov>
2025-03-06 23:41:53 -0700

    avoid using WMIC since it's deprecated

--- bin/resources/local.rb
+++ bin/resources/local.rb
@@ -65,8 +65,17 @@ def find_windows_pids(pid_json_path, recursion_limit = 6)
   pid_array += pid_hash[:dj_pids] if pid_hash[:dj_pids]
   pid_array << pid_hash[:rails_pid] if pid_hash[:rails_pid]
   pid_list = pid_array.clone
-  pid_str = `WMIC PROCESS get Caption,ProcessId,ParentProcessId`.split("\n\n")
-  pid_str.shift
+  if Gem.win_platform?
+    # Use PowerShell instead of deprecated WMIC
+    pid_str = `powershell -command "Get-Process | Select-Object ProcessName,Id,ParentProcessId | Format-Table -AutoSize | Out-String -Width 4096"`.split("\n")
+    # Remove header lines (column names and separator)
+    3.times { pid_str.shift }
+    # Remove empty lines
+    pid_str.reject! { |line| line.strip.empty? }
+  else
+    # For non-Windows platforms, use a different approach if needed
+    pid_str = []
+  end
   fully_recursed = false
   recursion_level = 0
   until fully_recursed
