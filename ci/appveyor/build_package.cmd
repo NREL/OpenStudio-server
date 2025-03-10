836cecd19ca677d40d4f8c79be7a959365b0901e
Brian Ball <brian.ball@nrel.gov>
2025-03-10 14:23:51 -0500

    try removing hanging CMD.exe

--- ci/appveyor/build_package.cmd
+++ ci/appveyor/build_package.cmd
@@ -9,8 +9,8 @@ set OPENSTUDIO_TEST_EXE=C:\projects\openstudio\bin\openstudio
 REM set mongo_dir??
 cd c:\
 mkdir export
-echo removing conflicting gem directory
-rmdir /S /Q C:\projects\openstudio-server\gems
+echo removing any hanging CMDs
+taskkill /IM cmd.exe /F /T
 echo openstudio_meta install_gems --export
 ruby C:\projects\openstudio-server\bin\openstudio_meta install_gems --export="C:\export" --debug
 mv C:\export C:\projects\openstudio-server\export
