0e4a432c619d8ad0617a12e652743559063232c1
brianlball <brian.ball@nrel.gov>
2025-03-10 21:49:48 -0500

    more debugging

--- ci/appveyor/build_package.cmd
+++ ci/appveyor/build_package.cmd
@@ -14,14 +14,14 @@ echo Downloading Handle...
 curl -L -o handle.zip https://download.sysinternals.com/files/Handle.zip
 tar -xf handle.zip -C %TEMP%
 echo Checking for file locks...
-%TEMP%\handle.exe C:\projects\openstudio-server\gems\gems\json-2.10.1 > %TEMP%\handle_output.txt
-type %TEMP%\handle_output.txt
-echo generator.so
-%TEMP%\handle.exe C:\projects\openstudio-server\gems\gems\json-2.10.1\lib\json\ext\generator.so > %TEMP%\handle_output2.txt
-type %TEMP%\handle_output2.txt
+REM %TEMP%\handle.exe C:\projects\openstudio-server\gems\gems\json-2.10.1 > %TEMP%\handle_output.txt
+REM type %TEMP%\handle_output.txt
+REM echo generator.so
+REM %TEMP%\handle.exe C:\projects\openstudio-server\gems\gems\json-2.10.1\lib\json\ext\generator.so > %TEMP%\handle_output2.txt
+REM type %TEMP%\handle_output2.txt
 cd c:\
 mkdir export
 echo openstudio_meta install_gems --export
-ruby C:\projects\openstudio-server\bin\openstudio_meta install_gems --export="C:\export" --debug
-mv C:\export C:\projects\openstudio-server\export
-dir C:\projects\openstudio-server\export
+REM ruby C:\projects\openstudio-server\bin\openstudio_meta install_gems --export="C:\export" --debug
+REM mv C:\export C:\projects\openstudio-server\export
+REM dir C:\projects\openstudio-server\export
