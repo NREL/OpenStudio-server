2e456cafa8648c489d1af77feb7e7087baf235f8
brianlball <brian.ball@nrel.gov>
2025-04-28 17:31:24 -0500

    try downgrading UCRT64 GCC from 15 to 14

--- ci/appveyor/setup.cmd
+++ ci/appveyor/setup.cmd
@@ -1,4 +1,13 @@
 @echo off
+rem ─── Downgrade UCRT64 GCC from 15 to 14 for racc native‐extensions ───
+ridk exec pacman --noconfirm -R mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-gcc-libs
+echo Downloading GCC 14 packages…
+curl -L -o gcc14.pkg.tar.zst ^
+  https://repo.msys2.org/mingw/ucrt64/x86_64/mingw-w64-ucrt-x86_64-gcc-14.3.0-1-any.pkg.tar.zst
+curl -L -o gcc14-libs.pkg.tar.zst ^
+  https://repo.msys2.org/mingw/ucrt64/x86_64/mingw-w64-ucrt-x86_64-gcc-libs-14.3.0-1-any.pkg.tar.zst
+ridk exec pacman --noconfirm -U gcc14-libs.pkg.tar.zst gcc14.pkg.tar.zst
+
 REM Set initial PATH with Git, Ruby binaries, and DevKit
 set PATH=C:\Ruby32-x64\bin;C:\DevKit\bin;C:\Program Files\Git\mingw64\bin;C:\projects\openstudio\bin;%PATH%
 
