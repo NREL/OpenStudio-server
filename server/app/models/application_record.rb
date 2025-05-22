a2248a8ee350e83e1a2a495131ac74c931f6c026
brianlball <brian.ball@nrel.gov>
2025-05-21 22:24:01 -0500

    delete application_record.rb since not used

--- server/app/models/application_record.rb
+++
@@ -1,9 +0,0 @@
-# *******************************************************************************
-# OpenStudio(R), Copyright (c) Alliance for Sustainable Energy, LLC.
-# See also https://openstudio.net/license
-# *******************************************************************************
-
-# This is not needed for Rails 5.1 because we are not using ActiveRecord because of mongoid
-# class ApplicationRecord < ActiveRecord::Base
-#   self.abstract_class = true
-# end
