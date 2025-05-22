799880ccd116c368f168041e4854078abf62e2e6
brianlball <brian.ball@nrel.gov>
2025-05-21 22:25:47 -0500

    use Rails 6.1 defaults

--- server/config/application.rb
+++ server/config/application.rb
@@ -22,7 +22,7 @@ module OpenstudioServer
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.load_defaults 6.1
     # Custom directories with classes and modules you want to be autoloadable.
     config.eager_load_paths << Rails.root.join('lib')
 
