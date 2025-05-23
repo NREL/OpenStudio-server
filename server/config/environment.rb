5662d36205c8966ad5b7c6557a13356d33e4ef58
brianlball <brian.ball@nrel.gov>
2025-05-23 13:57:26 -0500

    rails 7 app:upgrade

--- server/config/environment.rb
+++ server/config/environment.rb
@@ -4,7 +4,7 @@
 # *******************************************************************************
 
 # Load the Rails application.
-require File.expand_path('application', __dir__)
+require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
--- server/config/initializers/filter_parameter_logging.rb
+++ server/config/initializers/filter_parameter_logging.rb
@@ -6,4 +6,14 @@
 # Be sure to restart your server when you modify this file.
 # Configure sensitive parameters which will be filtered from the log file.
 #Rails.application.config.filter_parameters += [:password, /\Aanalysis(?!\_)/]
-Rails.application.config.filter_parameters += [:password, /\Aanalysis(?!\_)/, :file]
+#Rails.application.config.filter_parameters += [:password, /\Aanalysis(?!\_)/, :file]
+
+# Be sure to restart your server when you modify this file.
+
+# Configure parameters to be filtered from the log file. Use this to limit dissemination of
+# sensitive information. See the ActiveSupport::ParameterFilter documentation for supported
+# notations and behaviors.
+Rails.application.config.filter_parameters += [
+  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn,
+  :password, /\Aanalysis(?!\_)/, :file
+]
