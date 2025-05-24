b2107b22a30e6b3b22be22587b25ecd183c3bf41
brianlball <brian.ball@nrel.gov>
2025-05-24 14:56:27 -0500

    capybara.server= :webrick

--- server/spec/rails_helper.rb
+++ server/spec/rails_helper.rb
@@ -13,6 +13,9 @@ require 'rspec/rails'
 
 require 'capybara/rspec'
 require 'capybara/rails'
+require 'webrick'
+
+Capybara.server = :webrick
 
 # Capybara.app_host = 'http://localhost:3000'
 # Capybara.server_host = 'localhost'
