502ff307cd2ff0372895b6b0e66e82f98ee36cd4
brianlball <brian.ball@nrel.gov>
2025-05-30 08:22:10 -0500

    dockerhub build

--- docker/deployment/scripts/deploy_docker_github_actions.sh
+++ docker/deployment/scripts/deploy_docker_github_actions.sh
@@ -12,8 +12,8 @@ elif [ "${GITHUB_REF}" == "refs/heads/master" ]; then
     IMAGETAG="$(ruby -e "load 'server/app/lib/openstudio_server/version.rb'; print OpenstudioServer::Version+OpenstudioServer::VERSION_EXT")"
 # Uncomment and set branch name for custom builds. 
 # Currently setting this to setup_github_actions to test upload. 
-elif [ "${GITHUB_REF}" == "refs/heads/setup_github_actions" ]; then
-    IMAGETAG=experimental
+elif [ "${GITHUB_REF}" == "refs/heads/rails72" ]; then
+    IMAGETAG=rails7
 elif [ "${GITHUB_REF}" == "refs/heads/3.9.0" ]; then
      IMAGETAG="3.9.0"
 fi
