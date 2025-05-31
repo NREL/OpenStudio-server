eef788dabc0f268b78c0cb5903250266a178c98b
brianlball <brian.ball@nrel.gov>
2025-05-31 09:44:58 -0500

    error check OS windows installer script

--- ci/appveyor/setup.cmd
+++ ci/appveyor/setup.cmd
@@ -25,9 +25,31 @@ REM  “Unblock” the file so Windows does not refuse to execute it
 REM powershell -Command "Unblock-File -Path '%CD%\%OS_INSTALL_NAME%'"
 
 REM Execute the OpenStudio installer
-%OS_INSTALL_NAME% --script ci/appveyor/install-windows.qs
+REM %OS_INSTALL_NAME% --script ci/appveyor/install-windows.qs
+REM  3) Run the OpenStudio installer in “quiet” mode, pointing to our QScript
+echo Launching installer…
+REM  Use “.\” to ensure we’re running the downloaded EXE in the current directory
+.\%OS_INSTALL_NAME% --script ci/appveyor/install-windows.qs
+if %ERRORLEVEL% neq 0 (
+  echo.
+  echo ERROR: OpenStudio installer "%OS_INSTALL_NAME%" returned error code %ERRORLEVEL%. Aborting.
+  exit /b 1
+)
 
-move C:\openstudio C:\projects\openstudio
+REM move C:\openstudio C:\projects\openstudio
+REM  4) Move the default “C:\openstudio” install directory into the projects dir
+if exist C:\openstudio (
+  move /Y C:\openstudio C:\projects\openstudio
+  if %ERRORLEVEL% neq 0 (
+    echo.
+    echo ERROR: Could not move “C:\openstudio” to “C:\projects\openstudio”. Check permissions.
+    exit /b 1
+  )
+) else (
+  echo.
+  echo ERROR: After running the installer, “C:\openstudio” was not found. Aborting.
+  exit /b 1
+)
 dir C:\projects\openstudio
 
 REM Cleanup installer
